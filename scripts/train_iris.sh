58e5b51ad1a27f0cf0e1d6d2454afb0c933abb9e
Christopher Beckham <chrispy645@gmail.com>
2016-01-20 17:12:45 -0500

    Fix Iris script

--- scripts/train_iris.sh
+++ scripts/train_iris.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 java -Xmx5g -cp ../build/classes/:$WEKA_HOME/weka.jar:/Users/cjb60/github/weka-fresh/build/testcases/:../lib/* weka.Run \
-     .ChrisDL4JClassifier \
+     .Dl4jMlpClassifier \
      -S 0 \
-     -layer "weka.dl4j.layers.DenseLayer -num_units 3 -activation tanh -weight_init XAVIER" \
-     -layer "weka.dl4j.layers.OutputLayer -activation softmax -weight_init XAVIER -loss MCXENT" \
-     -num_iters 100 \
+     -layer "weka.dl4j.layers.DenseLayer -units 10 -activation tanh -init XAVIER" \
+     -layer "weka.dl4j.layers.OutputLayer -units 3 -activation softmax -init XAVIER -loss MCXENT" \
+     -iters 100 \
      -optim STOCHASTIC_GRADIENT_DESCENT \
-     -updater SGD \
-     -learning_rate 0.1 \
+     -updater NESTEROVS \
+     -lr 0.1 \
      -momentum 0.9 \
+     -bs 1 \
      -t ../datasets/iris.arff \
      -no-cv
