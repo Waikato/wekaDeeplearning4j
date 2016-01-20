f29a2f7713654fbf57ec1bf9b85a77f507a4925c
Christopher Beckham <chrispy645@gmail.com>
2016-01-20 16:52:47 -0500

    Cleanup

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ant -f build_package.xml make_package -Dpackage=ChrisDL4J
-cd dist && java weka.core.WekaPackageManager -install-package ChrisDL4J0.0.1.zip
+mvn clean
+mvn install
+ant clean
+ant -f make_package -Dpackage=wekaDl4j
+cd dist
+java weka.core.WekaPackageManager -install-package wekaDl4j0.0.1.zip
--- src/test/java/weka/classifiers/functions/DL4JClassifierTest.java
+++ src/test/java/weka/classifiers/functions/DL4JClassifierTest.java
@@ -1,37 +1,41 @@
 package weka.classifiers.functions;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class DL4JClassifierTest {
 	
-	public static void denseTest() throws Exception {
-		
+	public Instances loadIris() throws Exception {
 		DataSource ds = new DataSource("datasets/iris.arff");
 		Instances data = ds.getDataSet();
 		data.setClassIndex( data.numAttributes() - 1 );
-		
-		ChrisDL4JClassifier cls = new ChrisDL4JClassifier();
+		return data;
+	}
+	
+	@Test
+	public void denseTest() throws Exception {	
+		Instances data = loadIris();
+		Dl4jMlpClassifier cls = new Dl4jMlpClassifier();
 		cls.setLayers(new weka.dl4j.layers.Layer[] {
 				new weka.dl4j.layers.DenseLayer(),
 				new weka.dl4j.layers.DenseLayer(),
 				new weka.dl4j.layers.OutputLayer() 
 		});
 		cls.setTrainBatchSize(1);
-		cls.setDebugFile("/tmp/debug.txt");
+		//cls.setDebugFile("/tmp/debug.txt");
 		cls.buildClassifier(data);
-		
 		cls.distributionsForInstances(data);
-		
 	}
 	
-	public static void convTest() throws Exception {
+	@Ignore
+	public void convTest() throws Exception {
 		
-		DataSource ds = new DataSource("datasets/iris.arff");
-		Instances data = ds.getDataSet();
-		data.setClassIndex( data.numAttributes() - 1 );
+		Instances data = loadIris();
 		
-		ChrisDL4JClassifier cls = new ChrisDL4JClassifier();
+		Dl4jMlpClassifier cls = new Dl4jMlpClassifier();
 		cls.setLayers(new weka.dl4j.layers.Layer[] {
 				new weka.dl4j.layers.Conv1DLayer(),
 				new weka.dl4j.layers.OutputLayer() 
@@ -41,14 +45,6 @@ public class DL4JClassifierTest {
 		cls.buildClassifier(data);
 		
 		cls.distributionsForInstances(data);				
-		
-	}
-	
-	public static void main(String[] args) throws Exception {
-		
-		//denseTest();
-		
-		convTest();
 		
 	}
 
