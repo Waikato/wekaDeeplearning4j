b67bcba66ae76b8e610cc48ee732eb27e92422c8
Christopher Beckham <chrispy645@gmail.com>
2016-01-20 17:04:56 -0500

    Add gui image and fix build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 mvn clean
 mvn install
-ant clean
-ant -f make_package -Dpackage=wekaDl4j
+ant -f build_package.xml clean
+ant -f build_package.xml make_package -Dpackage=wekaDl4j
 cd dist
 java weka.core.WekaPackageManager -install-package wekaDl4j0.0.1.zip
