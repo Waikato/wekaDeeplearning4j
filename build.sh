41a3dcb5207b3cf551a86dea7096d48898818c12
Christopher Beckham <chrispy645@gmail.com>
2016-01-21 23:33:56 -0500

    Add WEKA test jar, enhanced build script

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-mvn clean
-mvn install
-ant -f build_package.xml clean
+if [ -z $WEKA_HOME ]; then
+    echo "make sure WEKA_HOME env variable is set!"
+    exit 1
+fi
+
+export CLASSPATH=$WEKA_HOME/weka.jar
+
+if [ $1 == "fresh" ]; then
+    mvn clean
+fi
+mvn -Dmaven.test.skip=true install
+if [ $1 == "fresh" ]; then
+    ant -f build_package.xml clean
+fi
 ant -f build_package.xml make_package -Dpackage=wekaDl4j
 cd dist
 java weka.core.WekaPackageManager -install-package wekaDl4j0.0.1.zip
