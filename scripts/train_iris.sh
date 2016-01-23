8b27ce590afa9abc01cf68fef01cbb709df45e8d
Christopher Beckham <chrispy645@gmail.com>
2016-01-22 20:05:33 -0500

    Update shell script examples

--- scripts/train_iris.sh
+++ scripts/train_iris.sh
@@ -5,11 +5,10 @@ java -Xmx5g -cp $WEKA_HOME/weka.jar weka.Run \
      -S 0 \
      -layer "weka.dl4j.layers.DenseLayer -units 10 -activation tanh -init XAVIER" \
      -layer "weka.dl4j.layers.OutputLayer -units 3 -activation softmax -init XAVIER -loss MCXENT" \
-     -iters 100 \
+     -iterator "weka.dl4j.iterators.DefaultDataSetIterator -bs 1 -iters 10" \
      -optim STOCHASTIC_GRADIENT_DESCENT \
      -updater NESTEROVS \
      -lr 0.1 \
      -momentum 0.9 \
-     -bs 1 \
      -t ../datasets/iris.arff \
      -no-cv
