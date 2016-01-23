81358a8aceab8a13ef920f34ca581a69f116fc87
Christopher Beckham <chrispy645@gmail.com>
2016-01-22 20:05:33 -0500

    Update shell script examples

--- scripts/train_diabetes.sh
+++ scripts/train_diabetes.sh
@@ -5,11 +5,10 @@ java -Xmx5g -cp $WEKA_HOME/weka.jar weka.Run \
      -S 0 \
      -layer "weka.dl4j.layers.DenseLayer -units 2 -activation relu -init XAVIER" \
      -layer "weka.dl4j.layers.OutputLayer -units 1 -activation identity -init XAVIER -loss MSE" \
-     -iters 100 \
+     -iterator "weka.dl4j.iterators.DefaultDataSetIterator -iters 100 -bs 100" \
      -optim STOCHASTIC_GRADIENT_DESCENT \
      -updater NESTEROVS \
      -lr 0.01 \
      -momentum 0.9 \
-     -bs 100 \
      -t ../datasets/diabetes_numeric.arff \
      -no-cv
