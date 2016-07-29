981a3bf4ea30a0472ea1293ce34878dfca91c1d9
Christopher Beckham <chrispy645@gmail.com>
2016-07-29 13:33:05 -0400

    Experimental build setting is turned off by default (it's a bit messy)

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ fi
 export CLASSPATH=$WEKA_HOME/weka.jar
 echo "classpath" $CLASSPATH
 
+DL4J_BUILD_EXPERIMENTAL="0"
+
 if [ $1 == "fresh" ]; then
     echo "removing contents in lib-full and lib"
     rm lib-full/*
@@ -19,20 +21,26 @@ fi
 mvn -Dmaven.test.skip=true install
 # clean-up
 ant -f build_package.xml clean
-# copy only the *necessary* jars from lib-full to lib
-while read p; do
-  if [ -e lib-full/$p ]; then
-    cp lib-full/$p lib/$p
-  fi
-  # we'll probably need all the core jars
-  cp lib-full/deeplearning*.jar lib/
-  # ...and apache
-  cp lib-full/commons*.jar lib/
-done <scripts/find_jars/jars_unique.txt
+
+if [ DL4J_BUILD_EXPERIMENTAL == "1" ]; then
+    # copy only the *necessary* jars from lib-full to lib
+    while read p; do
+      if [ -e lib-full/$p ]; then
+	  cp lib-full/$p lib/$p
+      fi
+      # we'll probably need all the core jars
+      cp lib-full/deeplearning*.jar lib/
+      # ...and apache
+      cp lib-full/commons*.jar lib/
+    done <scripts/find_jars/jars_unique.txt
+else
+    # just copy all the jars :(
+    cp lib-full/*jar lib/
+fi
 
 
 # build the package
 ant -f build_package.xml make_package -Dpackage=wekaDl4j
 
-#cd dist
-#java weka.core.WekaPackageManager -install-package wekaDl4j0.0.1.zip
+cd dist
+java weka.core.WekaPackageManager -install-package wekaDl4j0.0.1.zip
