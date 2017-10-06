c26b807ab4c8619fce1b2de293818378ddc6abd7
Steven Lang <slang03@students.uni-mainz.de>
2017-10-06 13:20:49 +1300

    Update train scripts for iris, diabetes and mnist runs

--- scripts/train_iris.sh
+++ scripts/train_iris.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-java -Xmx5g -cp $WEKA_HOME/weka.jar weka.Run \
-     .Dl4jMlpClassifier \
-     -S 0 \
-     -layer "weka.dl4j.layers.DenseLayer -units 10 -activation tanh -init XAVIER" \
-     -layer "weka.dl4j.layers.OutputLayer -units 3 -activation softmax -init XAVIER -loss MCXENT" \
-     -iterator "weka.dl4j.iterators.DefaultDataSetIterator -bs 150 -iters 10" \
-     -optim STOCHASTIC_GRADIENT_DESCENT \
-     -updater NESTEROVS \
-     -lr 0.1 \
-     -momentum 0.9 \
+java -Xmx5g -cp ${WEKA_HOME}/weka.jar weka.Run \
+     .Dl4jMlpClassifier -S 1 -iterator "weka.dl4j.iterators.DefaultInstancesIterator -bs 1" \
+     -normalization "Standardize training data" \
+     -layer "weka.dl4j.layers.OutputLayer -activation \"weka.dl4j.activations.ActivationSoftmax \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -L1 0.0 -L2 0.0 -name \"Output layer\" -lr 0.01 -lossFn \"weka.dl4j.lossfunctions.LossMCXENT \" -momentum 0.9 -rho 0.0 -rmsDecay 0.95 -updater NESTEROVS -weightInit XAVIER" \
+     -config "weka.dl4j.NeuralNetConfiguration -leakyreluAlpha 0.01 -learningRatePolicy None -lrPolicyDecayRate NaN -lrPolicyPower NaN -lrPolicySteps NaN -maxNumLineSearchIterations 5 -minimize -numIterations 1 -algorithm STOCHASTIC_GRADIENT_DESCENT -stepFunction \"weka.dl4j.stepfunctions.NegativeGradientStepFunction \" -useRegularization" \
+     -numEpochs 10 \
+     -queueSize 0 \
      -t ../datasets/nominal/iris.arff \
      -no-cv
--- scripts/train_mnist.sh
+++ scripts/train_mnist.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-java -Xmx5g -cp $WEKA_HOME/weka.jar weka.Run \
-     .Dl4jMlpClassifier -S 1 \
-     -layer "weka.dl4j.layers.Conv2DLayer -num_filters 16 -filter_size_x 3 -filter_size_y 3 -stride_x 1 -stride_y 1 -activation relu -init XAVIER" \
-     -layer "weka.dl4j.layers.Pool2DLayer -pool_size_x 2 -pool_size_y 2 -stride_x 2 -stride_y 2 -pool_type max" \
-     -layer "weka.dl4j.layers.Conv2DLayer -num_filters 32 -filter_size_x 3 -filter_size_y 3 -stride_x 1 -stride_y 1 -activation relu -init XAVIER" \
-     -layer "weka.dl4j.layers.Pool2DLayer -pool_size_x 2 -pool_size_y 2 -stride_x 2 -stride_y 2 -pool_type max" \
-     -layer "weka.dl4j.layers.Conv2DLayer -num_filters 48 -filter_size_x 3 -filter_size_y 3 -stride_x 1 -stride_y 1 -activation relu -init XAVIER" \
-     -layer "weka.dl4j.layers.Pool2DLayer -pool_size_x 2 -pool_size_y 2 -stride_x 2 -stride_y 2 -pool_type max" \
-     -layer "weka.dl4j.layers.DenseLayer -units 128 -activation relu -init XAVIER" \
-     -layer "weka.dl4j.layers.OutputLayer -units 10 -activation softmax -init XAVIER -p 0.0 -l1 0.0 -l2 0.0 -loss MCXENT" \
-     -iterator "weka.dl4j.iterators.ImageDataSetIterator -bs 128 -iters 2 -width 28 -height 28 -channels 1 -location ../mnist-data/" \
-     -optim STOCHASTIC_GRADIENT_DESCENT \
-     -lr 0.01 -momentum 0.9 -updater NESTEROVS -debug /tmp/debug.txt \
-     -output-debug-info \
-     -t ../datasets/nominal/mnist.meta.arff \
-     -batch-size 100 \
-     -no-cv
+java -Xmx5g -cp ${WEKA_HOME}/weka.jar weka.Run \
+     .Dl4jMlpClassifier \
+     -S 1 \
+     -iterator "weka.dl4j.iterators.ImageDataSetIterator -height 28 -imagesLocation /home/tak/WaikatoProjects/wekaDeeplearning4j/datasets/nominal/mnist-minimal -numChannels 1 -bs 1 -width 28" \
+     -normalization "Standardize training data" -layer "weka.dl4j.layers.ConvolutionLayer -nFilters 32 -activation \"weka.dl4j.activations.ActivationReLU \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -mode Truncate -cudnnAlgoMode PREFER_FASTEST -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -kernelSizeX 3 -kernelSizeY 3 -L1 0.0 -L2 0.0 -name \"Convolution layer\" -lr 0.01 -momentum 0.9 -paddingX 0 -paddingY 0 -rho 0.0 -rmsDecay 0.95 -strideX 1 -strideY 1 -updater ADAM -weightInit XAVIER" \
+     -layer "weka.dl4j.layers.SubsamplingLayer -activation \"weka.dl4j.activations.ActivationIdentity \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -mode Truncate -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -eps 1.0E-8 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -kernelSizeX 2 -kernelSizeY 2 -L1 0.0 -L2 0.0 -name \"Subsampling layer\" -lr 0.01 -momentum 0.9 -paddingX 0 -paddingY 0 -pnorm 1 -poolingType MAX -rho 0.0 -rmsDecay 0.95 -strideX 1 -strideY 1 -updater ADAM -weightInit XAVIER" \
+     -layer "weka.dl4j.layers.ConvolutionLayer -nFilters 16 -activation \"weka.dl4j.activations.ActivationReLU \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -mode Truncate -cudnnAlgoMode PREFER_FASTEST -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -kernelSizeX 3 -kernelSizeY 3 -L1 0.0 -L2 0.0 -name \"Convolution layer\" -lr 0.01 -momentum 0.9 -paddingX 0 -paddingY 0 -rho 0.0 -rmsDecay 0.95 -strideX 1 -strideY 1 -updater ADAM -weightInit XAVIER" \
+     -layer "weka.dl4j.layers.SubsamplingLayer -activation \"weka.dl4j.activations.ActivationIdentity \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -mode Truncate -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -eps 1.0E-8 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -kernelSizeX 2 -kernelSizeY 2 -L1 0.0 -L2 0.0 -name \"Subsampling layer\" -lr 0.01 -momentum 0.9 -paddingX 0 -paddingY 0 -pnorm 1 -poolingType MAX -rho 0.0 -rmsDecay 0.95 -strideX 1 -strideY 1 -updater ADAM -weightInit XAVIER" \
+     -layer "weka.dl4j.layers.OutputLayer -activation \"weka.dl4j.activations.ActivationSoftmax \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -L1 0.0 -L2 0.0 -name \"Output layer\" -lr 0.01 -lossFn \"weka.dl4j.lossfunctions.LossMCXENT \" -momentum 0.9 -rho 0.0 -rmsDecay 0.95 -updater NESTEROVS -weightInit XAVIER" \
+     -config "weka.dl4j.NeuralNetConfiguration -leakyreluAlpha 0.01 -learningRatePolicy None -lrPolicyDecayRate NaN -lrPolicyPower NaN -lrPolicySteps NaN -maxNumLineSearchIterations 5 -minimize -numIterations 1 -algorithm STOCHASTIC_GRADIENT_DESCENT -stepFunction \"weka.dl4j.stepfunctions.NegativeGradientStepFunction \" -useRegularization" \
+     -numEpochs 10 -queueSize 0 -batch-size 1 \
+    -t ../datasets/nominal/mnist.meta.minimal.arff \
+    -no-cv
