2aa1a97db212ebd51a3e95bbd3914c395c87dad2
Steven Lang <slang03@students.uni-mainz.de>
2017-10-06 16:05:10 +1300

    Init empty test class for dl4j word2vec

---
+++ wekaDeeplearning4jNLP/src/test/java/weka/filters/unsupervised/attribute/Dl4jStringToWord2VecTest.java
@@ -0,0 +1,21 @@
+package weka.filters.unsupervised.attribute;
+
+import org.junit.Test;
+
+import java.io.File;
+/**
+ * JUnit tests for the Dl4jMlpClassifier.
+ * Tests nominal classes with iris, numerical classes with diabetes and image
+ * classification with minimal mnist.
+ *
+ * @author Steven Lang
+ *
+ * @version $Revision: 11711 $
+ */
+public class Dl4jStringToWord2VecTest {
+    
+    @Test
+    public void testReuters(){
+        final String arffPath = "../datasets/text/ReutersCorn-train.arff";
+    }
+}
