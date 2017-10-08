98871d135aa81241be519b71c18946dca5002038
Steven Lang <slang03@students.uni-mainz.de>
2017-10-09 09:07:08 +1300

    Update minimal NLP test

--- wekaDeeplearning4jNLP/src/test/java/weka/filters/unsupervised/attribute/Dl4jStringToWord2VecTest.java
+++ wekaDeeplearning4jNLP/src/test/java/weka/filters/unsupervised/attribute/Dl4jStringToWord2VecTest.java
@@ -1,6 +1,9 @@
 package weka.filters.unsupervised.attribute;
 
 import org.junit.Test;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+import weka.filters.Filter;
 
 import java.io.File;
 /**
@@ -15,7 +18,12 @@ import java.io.File;
 public class Dl4jStringToWord2VecTest {
     
     @Test
-    public void testReuters(){
+    public void testReuters() throws Exception {
         final String arffPath = "../datasets/text/ReutersCorn-train.arff";
+        ConverterUtils.DataSource ds = new ConverterUtils.DataSource(arffPath);
+        final Instances data = ds.getDataSet();
+        Dl4jStringToWord2Vec dl4jw2v = new Dl4jStringToWord2Vec();
+        dl4jw2v.setInputFormat(data);
+        Instances d = Filter.useFilter(data, dl4jw2v);
     }
 }
