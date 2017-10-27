5d1c296d36d40432d082f2a17ee8353b012acbd9
Steven Lang <steven.lang.mz@gmail.com>
2017-10-27 17:33:57 +1300

    Fix train scripts

--- scripts/train_mnist_single_layer.sh
+++ scripts/train_mnist_single_layer.sh
@@ -3,8 +3,8 @@
 java -Xmx5g -cp ${WEKA_HOME}/weka.jar weka.Run \
      .Dl4jMlpClassifier \
      -S 1 \
-     -iterator "weka.dl4j.iterators.ImageDataSetIterator -height 28 -imagesLocation ../wekaDeeplearning4jCore/datasets/nominal/mnist-minimal -numChannels 1 -bs 64 -width 28" \
+     -iterator "weka.dl4j.iterators.instance.ImageInstanceIterator -height 28 -imagesLocation ../wekaDeeplearning4jCore/datasets/nominal/mnist-minimal -numChannels 1 -bs 64 -width 28" \
      -layer "weka.dl4j.layers.OutputLayer -activation \"weka.dl4j.activations.ActivationSoftmax \" -adamMeanDecay 0.9 -adamVarDecay 0.999 -biasInit 1.0 -l1Bias 0.0 -l2Bias 0.0 -blr 0.01 -dist \"weka.dl4j.distribution.NormalDistribution -mean 0.001 -std 1.0\" -dropout 0.0 -epsilon 1.0E-6 -gradientNormalization None -gradNormThreshold 1.0 -L1 0.01 -L2 0.0 -name \"Output layer\" -lr 0.01 -lossFn \"weka.dl4j.lossfunctions.LossMCXENT \" -momentum 0.9 -rho 0.0 -rmsDecay 0.95 -updater ADAM -weightInit XAVIER" \
-     -numEpochs 2 -queueSize 0 -batch-size 1 \
+     -numEpochs 10 \
      -t ../wekaDeeplearning4jCore/datasets/nominal/mnist.meta.minimal.arff \
      -no-cv
