11aac100d80ae52c0a46f31779e23178d061de88
Steven Lang <steven.lang.mz@gmail.com>
2017-11-03 17:12:25 +1300

    Use alexnet in alexnet script

--- scripts/alexnet.sh
+++ scripts/alexnet.sh
@@ -5,7 +5,7 @@ java -Xmx5g -cp ${WEKA_HOME}/weka.jar weka.Run \
      -S 1 \
      -iterator "weka.dl4j.iterators.instance.ImageInstanceIterator -height 32 -imagesLocation /home/slang/datasets/cifar10/train -numChannels 1 -bs 128 -width 32" \
      -normalization "Standardize training data" \
-     -zooModel "weka.dl4j.zoo.LeNet" \
+     -zooModel "weka.dl4j.zoo.AlexNet" \
      -iteration-listener "weka.dl4j.listener.EpochListener" \
      -numEpochs 10 \
      -t /home/slang/datasets/cifar10/cifar10.arff \
