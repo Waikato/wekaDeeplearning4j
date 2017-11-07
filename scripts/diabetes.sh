b70b7b10313c73829b3e5752bda3297051813298
Steven Lang <steven.lang.mz@gmail.com>
2017-11-07 14:54:32 +1300

    Update test scripts according to new learning rate setting

--- scripts/diabetes.sh
+++ scripts/diabetes.sh
@@ -6,7 +6,7 @@ java -Xmx5g -cp ${WEKA_HOME}/weka.jar weka.Run \
      -iterator "weka.dl4j.iterators.instance.DefaultInstanceIterator -bs 1" \
      -normalization "Standardize training data" \
      -layer "weka.dl4j.layers.DenseLayer -nOut 10 " \
-     -layer "weka.dl4j.layers.OutputLayer -activation \"weka.dl4j.activations.ActivationSoftmax \" -lossFn \"weka.dl4j.lossfunctions.LossMCXENT \" -updater NESTEROVS" \
+     -layer "weka.dl4j.layers.OutputLayer -activation \"weka.dl4j.activations.ActivationSoftmax \" -lossFn \"weka.dl4j.lossfunctions.LossMCXENT \"" \
      -numEpochs 10 \
      -t ../wekaDeeplearning4jCore/datasets/numeric/diabetes_numeric.arff \
      -split-percentage 66
--- scripts/mnist.sh
+++ scripts/mnist.sh
@@ -4,12 +4,11 @@ java -Xmx5g -cp ${WEKA_HOME}/weka.jar weka.Run \
      .Dl4jMlpClassifier \
 	 -S "42" \
 	 -normalization "Standardize training data" \
-	 -iterator "weka.dl4j.iterators.instance.ImageInstanceIterator -height 28 -width 28 -imagesLocation ../wekaDeeplearning4jCore/src/test/resources/nominal/mnist-minimal -numChannels 1 -bs 256 " \
-	 -layer "weka.dl4j.layers.ConvolutionLayer -nFilters 32 -kernelSizeX 3 -kernelSizeY 3 -strideX 1 -strideY 1" \
+	 -iterator "weka.dl4j.iterators.instance.ImageInstanceIterator -height 28 -width 28 -imagesLocation ../wekaDeeplearning4jCore/src/test/resources/nominal/mnist-minimal -numChannels 1 -bs 32 " \
+	 -layer "weka.dl4j.layers.ConvolutionLayer -nFilters 8 -kernelSizeX 3 -kernelSizeY 3 -strideX 1 -strideY 1" \
 	 -layer "weka.dl4j.layers.SubsamplingLayer -kernelSizeX 2 -kernelSizeY 2 -strideX 2 -strideY 2" \
-	 -layer "weka.dl4j.layers.ConvolutionLayer -nFilters 64 -kernelSizeX 3 -kernelSizeY 3 -strideX 1 -strideY 1" \
+	 -layer "weka.dl4j.layers.ConvolutionLayer -nFilters 8 -kernelSizeX 3 -kernelSizeY 3 -strideX 1 -strideY 1" \
 	 -layer "weka.dl4j.layers.SubsamplingLayer -kernelSizeX 2 -kernelSizeY 2 -strideX 2 -strideY 2" \
-	 -layer "weka.dl4j.layers.DenseLayer -nOut 512 -activation \"weka.dl4j.activations.ActivationReLU \" " \
 	 -layer "weka.dl4j.layers.OutputLayer " \
      -numEpochs 10 \
      -t ../wekaDeeplearning4jCore/datasets/nominal/mnist.meta.minimal.arff \
