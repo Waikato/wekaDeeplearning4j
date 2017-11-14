d3dfb5863fd01a8208d38c80faad3aa578adc48b
Steven Lang <steven.lang.mz@gmail.com>
2017-11-14 15:50:39 +1300

    Fix version bump script

--- version-bump.sh
+++ version-bump.sh
@@ -3,5 +3,5 @@
 old=$1
 new=$2
 
-sed -i.bak -e "0,/${old}/ s/${old}/${new}/" wekaDeeplearning4jCore/pom.xml
-sed -i.bak -e "0,/${old}/ s/${old}/${new}/" wekaDeeplearning4jCore/build_package.xml
+sed -i.bak -e "0,/${old}/ s/${old}/${new}/" package/pom.xml
+sed -i.bak -e "0,/${old}/ s/${old}/${new}/" package/build_package.xml
