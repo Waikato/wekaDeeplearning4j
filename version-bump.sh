0f61c3fc1e7701f861254414b4978dcfecf1cf8b
Steven Lang <steven.lang.mz@gmail.com>
2017-11-15 14:32:10 +1300

    Add commit in version bump script

--- version-bump.sh
+++ version-bump.sh
@@ -5,3 +5,7 @@ new=$2
 
 sed -i.bak -e "0,/${old}/ s/${old}/${new}/" package/pom.xml
 sed -i.bak -e "0,/${old}/ s/${old}/${new}/" package/build_package.xml
+
+git add package/pom.xml -v
+git add package/build_package.xml -v
+git commit -m "Version bump v${old} to v${new}"
