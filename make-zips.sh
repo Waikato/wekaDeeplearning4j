1517fac30dcffe8eab29c531767ba5b56a606e16
Steven Lang <steven.lang.mz@gmail.com>
2017-12-11 13:45:05 +1300

    Add sha256 sum generation to make-zips scripts

--- make-zips.sh
+++ make-zips.sh
@@ -6,4 +6,17 @@ mkdir /tmp/zips
 cp package/dist/*.zip /tmp/zips
 ./build.sh -c -v -b GPU
 cp package/dist/*.zip /tmp/zips
+old=$(pwd)
+cd /tmp/zips
+
+sumfile="/tmp/zips/sums.sha256"
+echo "### SHA256 sums" >> ${sumfile}
+echo "| sha256sum | file |" >> ${sumfile}
+echo "| --- | --- |" >> ${sumfile}
+for f in *.zip;
+do	
+	sum=$(sha256sum ${f} | cut -d" " -f1)
+	echo "| ${f} | ${sum} |" >> /tmp/zips/sums.sha256
+done
+cd ${old}
 
