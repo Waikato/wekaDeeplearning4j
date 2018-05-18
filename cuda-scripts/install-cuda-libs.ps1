2936383710689447bff3b682d727812cedbca593
Steven Lang <steven.lang.mz@gmail.com>
2018-05-18 17:03:02 +1200

    Add installation scripts for cuda libraries for linux/macosx and windows

---
+++ cuda-scripts/install-cuda-libs.ps1
@@ -0,0 +1,62 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Get all necessary variables
+$platform = "windows"
+$version = "1.5.0-alpha.1"
+$nvcc_output = $( nvcc --version )
+$cuda_version = (($nvcc_output -split ",")[1] -split " ")[2]
+$zip_name = "wekaDeeplearning4j-cuda-$cuda_version-$version-$platform-x86_64.zip"
+$url = "https://github.com/Waikato/wekaDeeplearning4j/releases/download/v$version/$zip_name"
+$output = $( Join-Path $PSScriptRoot $zip_name )
+
+
+
+
+# Set weka_home
+if (-not(Test-Path env:WEKA_HOME))
+{
+    $weka_home = Join-Path $( Get-Item Env:HOMEPATH ).Value "wekafiles"
+}
+else
+{
+    $weka_home = $( Get-Item Env:WEKA_HOME ).Value
+}
+
+# Check if package is installed
+if (-not(Test-Path [io.path]::combine($weka_home, "packages", "wekaDeeplearning4j")))
+{
+    Write-Output "Could not find $weka_home/packages/wekaDeeplearning4j. Is the wekaDeeplearning4j package installed?"
+    exit
+}
+
+# Check if zip is already downloaded
+if ($args.length > 0)
+{
+    $zip_name = $args[0]
+    Write-Output "Installing libraries from $zip_name. Skipping download ..."
+}
+elseif (Test-Path $zip_name)
+{
+    Write-Output "The file $zip_name already exists. Skipping download ..."
+}
+else
+{
+    # Download zip
+    Write-Output "Downloading $zip_name now ..."
+    $start_time = Get-Date
+    $wc = New-Object System.Net.WebClient
+    Invoke-WebRequest -Uri $url -OutFile $output
+    Write-Output "Time taken: $( (Get-Date).Subtract($start_time).Seconds ) second(s)"
+}
+
+
+$out_dir = $( Join-Path $PSScriptRoot "out" )
+# Unzip file
+Write-Output "Extracting the CUDA libraries ..."
+[System.IO.Compression.ZipFile]::ExtractToDirectory($output, $out_dir)
+$copy_from = [io.path]::combine($out_dir, "lib", "*")
+$copy_to = [io.path]::combine($weka_home, "packages", "wekaDeeplearning4j", "lib")
+Copy-Item -Path $copy_from -Destination $copy_to
+Remove-Item $out_dir -recurse
+Write-Output "Successfully installed the CUDA libraries to the wekaDeeplearning4j package!"
