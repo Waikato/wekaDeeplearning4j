18554d648d937266ee264285f91b73a8867963ba
Steven Lang <steven.lang.mz@gmail.com>
2018-05-18 17:13:55 +1200

    Update version bump script

--- version-bump.sh
+++ version-bump.sh
@@ -5,7 +5,11 @@ new=$2
 
 sed -i.bak -e "0,/${old}/ s/${old}/${new}/" package/pom.xml
 sed -i.bak -e "0,/${old}/ s/${old}/${new}/" version
+sed -i.bak -e "0,/${old}/ s/${old}/${new}/" cuda-scripts/install-cuda-libs.ps1
+sed -i.bak -e "0,/${old}/ s/${old}/${new}/" cuda-scripts/install-cuda-libs.sh
 
-#git add package/pom.xml -v
-#git add version -v
-#git commit -m "Version bump v${old} to v${new}"
+git add package/pom.xml -v
+git add version -v
+git add cuda-scripts/install-cuda-libs.ps1 -v
+git add cuda-scripts/install-cuda-libs.sh -v
+git commit -m "Version bump v${old} to v${new}"
