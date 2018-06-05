01f2201a091ab9be02a9bcb4aa9a69cae6eb2c19
Steven Lang <steven.lang.mz@gmail.com>
2018-06-06 11:56:48 +1200

    Add dummy dropout implementation in Disabled dropout class

--- src/main/java/weka/dl4j/dropout/Disabled.java
+++ src/main/java/weka/dl4j/dropout/Disabled.java
@@ -2,15 +2,27 @@ package weka.dl4j.dropout;
 
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.deeplearning4j.nn.conf.dropout.Dropout;
+import weka.dl4j.dropout.Disabled.DisabledDropoutImpl;
 
 @EqualsAndHashCode(callSuper = true)
 @ToString
-public class Disabled extends AbstractDropout<org.deeplearning4j.nn.conf.dropout.Dropout> {
+public class Disabled extends AbstractDropout<DisabledDropoutImpl> {
 
   private static final long serialVersionUID = 8082864981844682636L;
 
   @Override
   public void initializeBackend() {
     backend = null;
+  }
+
+  /**
+   * Dummy dropout implementation.
+   */
+  protected class DisabledDropoutImpl extends Dropout{
+    private static final long serialVersionUID = 5933930276882455322L;
+    public DisabledDropoutImpl(double activationRetainProbability) {
+      super(activationRetainProbability);
+    }
   }
 }
