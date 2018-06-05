ca9dee70d3532e43d4e98aca28a656bfe7574be2
Steven Lang <steven.lang.mz@gmail.com>
2018-06-06 11:55:31 +1200

    Add options to abstract feedforward layer class

--- src/main/java/weka/dl4j/layers/FeedForwardLayer.java
+++ src/main/java/weka/dl4j/layers/FeedForwardLayer.java
@@ -1,5 +1,7 @@
 package weka.dl4j.layers;
 
+import java.util.Enumeration;
+import weka.core.Option;
 import weka.core.OptionMetadata;
 import weka.dl4j.activations.Activation;
 
@@ -45,5 +47,35 @@ public abstract class FeedForwardLayer<T extends org.deeplearning4j.nn.conf.laye
 
   public void setNOut(int nOut) {
     backend.setNOut(nOut);
+  }
+
+  /**
+   * Returns an enumeration describing the available options.
+   *
+   * @return an enumeration of all the available options.
+   */
+  @Override
+  public Enumeration<Option> listOptions() {
+    return Option.listOptionsForClassHierarchy(this.getClass(), super.getClass()).elements();
+  }
+
+  /**
+   * Gets the current settings of the Classifier.
+   *
+   * @return an array of strings suitable for passing to setOptions
+   */
+  @Override
+  public String[] getOptions() {
+    return Option.getOptionsForHierarchy(this, super.getClass());
+  }
+
+  /**
+   * Parses a given list of options.
+   *
+   * @param options the list of options as an array of strings
+   * @throws Exception if an option is not supported
+   */
+  public void setOptions(String[] options) throws Exception {
+    Option.setOptionsForHierarchy(options, this, super.getClass());
   }
 }
