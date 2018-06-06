ee9f69f1db9f657a977a7816f2812df7bb2cb212
Steven Lang <steven.lang.mz@gmail.com>
2018-06-06 16:51:28 +1200

    Skip javadoc on tests

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,4 @@
 rm -r wekarefs
 
 # Disable global maven options
-MAVEN_OPTS="-ea -Xms1G -Xmx4G -Dorg.bytedeco.javacpp.maxbytes=12G -Dorg.bytedeco.javacpp.maxphysicalbytes=12G" mvn test -P cuda-9.1 -Dmaven.test.skip=false
+MAVEN_OPTS="-ea -Xms1G -Xmx4G -Dorg.bytedeco.javacpp.maxbytes=12G -Dorg.bytedeco.javacpp.maxphysicalbytes=12G" mvn test -P cuda-9.1 -Dmaven.javadoc.skip=true -Dmaven.test.skip=false
