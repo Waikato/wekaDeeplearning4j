e0a16d86819d804a01cd839c312732c9219e34a3
Steven Lang <steven.lang.mz@gmail.com>
2018-06-20 10:00:46 +1200

    Add options and backend to to all api wrapper tests

--- src/test/java/weka/dl4j/ApiWrapperTest.java
+++ src/test/java/weka/dl4j/ApiWrapperTest.java
@@ -1,6 +1,10 @@
 package weka.dl4j;
 
+import java.util.Arrays;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import weka.core.OptionHandler;
 
 /**
  * An abstract test class for api wrapper objects.
@@ -22,6 +26,30 @@ public abstract class ApiWrapperTest<T extends ApiWrapper> {
   @Before
   public void setup(){
     wrapper = getApiWrapper();
+  }
+
+  @Test
+  public void testOptions() throws Exception {
+    if (wrapper instanceof OptionHandler){
+      OptionHandler optionHandler = ((OptionHandler) wrapper);
+      String[] options = optionHandler.getOptions();
+      String[] optionsCopy = Arrays.copyOf(options, options.length);
+      optionHandler.setOptions(optionsCopy);
+      String[] optionsAfter = optionHandler.getOptions();
+
+      Assert.assertArrayEquals(options, optionsAfter);
+    }
+  }
+
+  @Test
+  public void testSettingBackends(){
+    T apiWrapper1 = getApiWrapper();
+    T apiWrapper2 = getApiWrapper();
+
+    apiWrapper2.setBackend(apiWrapper1.getBackend());
+
+    Assert.assertEquals(apiWrapper1, apiWrapper2);
+    Assert.assertEquals(apiWrapper1.getBackend(), apiWrapper2.getBackend());
   }
 
 }
