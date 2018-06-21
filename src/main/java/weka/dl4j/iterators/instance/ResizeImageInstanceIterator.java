665f7147f69b503de382ba1f9c23ea4519ae854a
Steven Lang <steven.lang.mz@gmail.com>
2018-06-21 15:00:26 +1200

    Fix resize image iterator

--- src/main/java/weka/dl4j/iterators/instance/ResizeImageInstanceIterator.java
+++ src/main/java/weka/dl4j/iterators/instance/ResizeImageInstanceIterator.java
@@ -21,11 +21,14 @@
 
 package weka.dl4j.iterators.instance;
 
+import java.io.File;
 import org.datavec.api.split.CollectionInputSplit;
 import org.datavec.image.recordreader.ImageRecordReader;
 import org.datavec.image.transform.ResizeImageTransform;
 import weka.core.Instances;
+import weka.core.OptionMetadata;
 import weka.dl4j.ArffMetaDataLabelGenerator;
+import weka.gui.ProgrammaticProperty;
 
 /**
  * An iterator that loads images and resizes them.
@@ -37,17 +40,118 @@ public class ResizeImageInstanceIterator extends ImageInstanceIterator {
   /** SerialVersionUID */
   private static final long serialVersionUID = -3310258401133869149L;
 
+  /** New image height */
+  protected int resizeHeight = 28;
+
+  /** New image width */
+  protected int resizeWidth = 28;
+
+  /** Backing image iterator */
+  protected ImageInstanceIterator iii = new ImageInstanceIterator();
+
+  /**
+   * Empty constructor for Weka
+   */
+  public ResizeImageInstanceIterator(){
+  }
+
+
+  @OptionMetadata(
+      displayName = "image iterator",
+      description = "The actual iterator used to load the images.",
+      commandLineParamName = "imageIterator",
+      commandLineParamSynopsis = "-imageIterator <ImageInstanceIterator>",
+      displayOrder = 1
+  )
+  public ImageInstanceIterator getImageInstanceIterator() {
+    return iii;
+  }
+
+  public void setImageInstanceIterator(ImageInstanceIterator iii) {
+    this.iii = iii;
+  }
+
+  @Override
+  @ProgrammaticProperty
+  public int getTrainBatchSize() {
+    return iii.getTrainBatchSize();
+  }
+
+  @Override
+  @ProgrammaticProperty
+  public void setTrainBatchSize(int trainBatchSize) {
+    iii.setTrainBatchSize(trainBatchSize);
+  }
+
+  @OptionMetadata(
+      displayName = "resize width",
+      description = "The width to resize the image to (default = 28).",
+      commandLineParamName = "resizeWidth",
+      commandLineParamSynopsis = "-resizeWidth <int>",
+      displayOrder = 2
+  )
+  @Override
+  public int getWidth() {
+    return resizeWidth;
+  }
+
+  @Override
+  public void setWidth(int width) {
+    resizeWidth = width;
+  }
+
+  @OptionMetadata(
+      displayName = "resize height",
+      description = "The height to resize the image to (default = 28).",
+      commandLineParamName = "resizeHeight",
+      commandLineParamSynopsis = "-resizeHeight <int>",
+      displayOrder = 3
+  )
+  @Override
+  public int getHeight() {
+    return resizeHeight;
+  }
+
+  @Override
+  public void setHeight(int height) {
+    resizeHeight = height;
+  }
+
+  @Override
+  @ProgrammaticProperty
+  public int getNumChannels() {
+    return iii.getNumChannels();
+  }
+
+  @Override
+  @ProgrammaticProperty
+  public void setNumChannels(int numChannels) {
+    iii.setNumChannels(numChannels);
+  }
+
+  @Override
+  @ProgrammaticProperty
+  public File getImagesLocation() {
+    return iii.getImagesLocation();
+  }
+
+  @Override
+  @ProgrammaticProperty
+  public void setImagesLocation(File imagesLocation) {
+    iii.setImagesLocation(imagesLocation);
+  }
+
   /**
    * Default constructor with the new shape
    *
    * @param iii Previous image iterator
-   * @param newWidth New image width
-   * @param newHeight New image height
+   * @param resizeWidth New image width
+   * @param resizeHeight New image height
    */
-  public ResizeImageInstanceIterator(ImageInstanceIterator iii, int newWidth, int newHeight) {
+  public ResizeImageInstanceIterator(ImageInstanceIterator iii, int resizeWidth, int resizeHeight) {
     super();
-    this.height = newHeight;
-    this.width = newWidth;
+    this.resizeHeight = resizeHeight;
+    this.resizeWidth = resizeWidth;
     this.setTrainBatchSize(iii.getTrainBatchSize());
     this.setImagesLocation(iii.getImagesLocation());
     this.setNumChannels(iii.getNumChannels());
@@ -57,7 +161,7 @@ public class ResizeImageInstanceIterator extends ImageInstanceIterator {
   protected ImageRecordReader getImageRecordReader(Instances data) throws Exception {
     ArffMetaDataLabelGenerator labelGenerator =
         new ArffMetaDataLabelGenerator(data, getImagesLocation().toString());
-    ResizeImageTransform rit = new ResizeImageTransform(width, height);
+    ResizeImageTransform rit = new ResizeImageTransform(getWidth(), getHeight());
     ImageRecordReader reader =
         new ImageRecordReader(getHeight(), getWidth(), getNumChannels(), labelGenerator, rit);
     CollectionInputSplit cis = new CollectionInputSplit(labelGenerator.getPathURIs());
