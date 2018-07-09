8bbb1fdce1a0c1cdfdd9748d4d42d142e9d0aa45
Steven Lang <steven.lang.mz@gmail.com>
2018-07-09 18:56:25 +0200

    Delete run-tests.sh

--- run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -r wekarefs
-
-# Disable global maven options
-MAVEN_OPTS="-ea -Xms1G -Xmx4G -Dorg.bytedeco.javacpp.maxbytes=12G -Dorg.bytedeco.javacpp.maxphysicalbytes=12G" mvn test -P cuda-9.1 -Dmaven.javadoc.skip=true -Dmaven.test.skip=false -Dweka.test.maventest=true
