a626e2c2a407dd6df35589a9325b0ce3958dfac1
Steven Lang <slang03@students.uni-mainz.de>
2018-08-07 13:02:42 +0200

    Add mixed class meta-data test

--- src/test/java/weka/classifiers/functions/DatasetTest.java
+++ src/test/java/weka/classifiers/functions/DatasetTest.java
@@ -1,14 +1,16 @@
 package weka.classifiers.functions;
 
+import java.io.File;
+import org.junit.Assert;
+import weka.classifiers.Evaluation;
 import weka.dl4j.activations.ActivationReLU;
 import weka.dl4j.activations.ActivationSoftmax;
-import weka.dl4j.layers.Layer;
+import weka.dl4j.iterators.instance.ImageInstanceIterator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
-import org.nd4j.linalg.activations.Activation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import weka.core.Instances;
@@ -16,8 +18,6 @@ import weka.dl4j.NeuralNetConfiguration;
 import weka.dl4j.layers.DenseLayer;
 import weka.dl4j.layers.OutputLayer;
 import weka.util.DatasetLoader;
-
-import java.io.IOException;
 
 /**
  * JUnit tests applying the classifier to different arff datasets.
@@ -117,5 +117,47 @@ public class DatasetTest {
 
     clf.buildClassifier(data);
     clf.distributionsForInstances(data);
+  }
+
+  /**
+   * Test datasets with class meta data that is not in lexicographic order.
+   *
+   * @throws Exception Something went wrong.
+   */
+  @Test
+  public void testMixedClassOrder() throws Exception {
+    String prefix = "src/test/resources/nominal/";
+
+    // Get data
+    Instances testProb = DatasetLoader.loadArff(prefix + "mnist.meta.minimal.arff");
+    Instances testProbInverse = DatasetLoader.loadArff(prefix + "mnist.meta.minimal.mixed-class-meta-data.arff");
+
+    Evaluation evalNormal = eval(testProb);
+    Evaluation evalMixed = eval(testProbInverse);
+
+    // Compare accuracy
+    Assert.assertEquals(evalNormal.pctCorrect(), evalMixed.pctCorrect(), 1e-7);
+    Assert.assertEquals(evalNormal.pctIncorrect(), evalMixed.pctIncorrect(), 1e-7);
+  }
+
+  private static Evaluation eval(Instances metaData)
+      throws Exception {
+
+    String imagesPath = "src/test/resources/nominal/mnist-minimal";
+    Dl4jMlpClassifier clf = new Dl4jMlpClassifier();
+    ImageInstanceIterator iii = new ImageInstanceIterator();
+    iii.setImagesLocation(new File(imagesPath));
+    iii.setTrainBatchSize(2);
+
+    clf.setInstanceIterator(iii);
+    clf.setNumEpochs(5);
+
+    // Build clf
+    clf.buildClassifier(metaData);
+
+    // Evaluate clf
+    Evaluation trainEval = new Evaluation(metaData);
+    trainEval.evaluateModel(clf, metaData);
+    return trainEval;
   }
 }
