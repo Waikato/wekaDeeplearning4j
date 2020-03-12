5dc3d376a2a1033ea75b3b2e2242fa7e0869a8b9
Rhys <rhys.compton@gmail.com>
2020-03-12 16:40:12 +1300

    Update DL4JMLPFilter to use new DL4J featurizing

--- src/main/java/weka/filters/unsupervised/attribute/Dl4jMlpFilter.java
+++ src/main/java/weka/filters/unsupervised/attribute/Dl4jMlpFilter.java
@@ -20,16 +20,22 @@ package weka.filters.unsupervised.attribute;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.Enumeration;
+
+import org.deeplearning4j.nn.transferlearning.TransferLearningHelper;
+import org.deeplearning4j.zoo.PretrainedType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.cpu.nativecpu.NDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.factory.Nd4j;
 import weka.classifiers.functions.Dl4jMlpClassifier;
-import weka.core.Instances;
-import weka.core.Option;
-import weka.core.OptionHandler;
-import weka.core.OptionMetadata;
-import weka.core.WekaException;
-import weka.core.WekaPackageManager;
+import weka.classifiers.functions.dl4j.Utils;
+import weka.core.*;
+import weka.dl4j.iterators.instance.ImageInstanceIterator;
+import weka.dl4j.zoo.AbstractZooModel;
+import weka.dl4j.zoo.ResNet50;
 import weka.filters.Filter;
 import weka.filters.SimpleBatchFilter;
 
@@ -41,12 +47,34 @@ import weka.filters.SimpleBatchFilter;
  */
 public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
 
-  private static final long serialVersionUID = 2125146603305868223L;
+  private static final long serialVersionUID = 21054165308223L;
 
   /**
    * The classifier model this filter is based on.
    */
   protected File serializedModelFile = new File(WekaPackageManager.getPackageHome().toURI());
+
+  /**
+   * The zoo model to use, if we're not loading from the serialized model file
+   */
+  protected AbstractZooModel zooModelType = new ResNet50().setPretrainedType(PretrainedType.IMAGENET);
+
+  /**
+   * The image instance iterator to use
+   */
+  protected ImageInstanceIterator imageInstanceIterator = new ImageInstanceIterator();
+
+  /**
+   * Is the supplied dataset a meta file (simply contains locations of image files)
+   * or does the arff contain all the image data inside
+   */
+  protected boolean isMetaArff = true;
+
+  /**
+   * Should we use the selected Model Zoo model, or should we use the serialized model
+   * as specified by specifiedModelFile
+   */
+  protected boolean useZooModel = true;
 
   protected Dl4jMlpClassifier model;
 
@@ -56,8 +84,8 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
   protected String transformationLayerName = "Output Layer";
 
   @OptionMetadata(
-      description = "Layer name of the layer used for the feature transformation.",
-      displayName = "layer name of the layer used for the feature transformation",
+      description = "Layer name of the layer used for the feature transformation",
+      displayName = "Feature extraction layer",
       commandLineParamName = "layerName",
       commandLineParamSynopsis = "-layerName <String>",
       displayOrder = 0
@@ -71,8 +99,8 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
   }
 
   @OptionMetadata(
-      description = "The trained Dl4jMlpClassifier object that contains the network.",
-      displayName = "model used for feature transformation",
+      description = "The trained Dl4jMlpClassifier object that contains the network, used for transformation.",
+      displayName = "Serialized model file",
       commandLineParamName = "model",
       commandLineParamSynopsis = "-model <File>",
       displayOrder = 1
@@ -85,6 +113,51 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
     this.serializedModelFile = modelPath;
   }
 
+  @OptionMetadata(
+          description = "The pretrained model from the DL4J Model Zoo (or a keras model)",
+          displayName = "Pretrained zoo model",
+          commandLineParamName = "zooModel",
+          commandLineParamSynopsis = "-zooModel <Model Zoo specification>",
+          displayOrder = 2
+  )
+  public AbstractZooModel getZooModelType() { return zooModelType; }
+
+  public void setZooModelType(AbstractZooModel zooModelType) { this.zooModelType = zooModelType; }
+
+  @OptionMetadata(
+          description = "Are the supplied instances 'meta instances' (just point to image file location)",
+          displayName = "Using 'meta instances'?",
+          commandLineParamName = "isMeta",
+          commandLineParamSynopsis = "-isMeta <true|false>",
+          displayOrder = 3
+  )
+  public boolean isMetaArff() { return isMetaArff; }
+
+  public void setMetaArff(boolean metaArff) { isMetaArff = metaArff; }
+
+  @OptionMetadata(
+          description = "Use the zoo model specification instead of the serialized model file",
+          displayName = "Use the zoo model",
+          commandLineParamName = "isZoo",
+          commandLineParamSynopsis = "-isZoo <true|false>",
+          displayOrder = 4
+  )
+  public boolean isUseZooModel() { return useZooModel; }
+
+  public void setUseZooModel(boolean useZooModel) { this.useZooModel = useZooModel; }
+
+  @OptionMetadata(
+          description = "The instance iterator to use.",
+          displayName = "instance iterator", commandLineParamName = "iterator",
+          commandLineParamSynopsis = "-iterator <string>"
+  )
+  public ImageInstanceIterator getImageInstanceIterator() {
+    return imageInstanceIterator;
+  }
+
+  public void setImageInstanceIterator(ImageInstanceIterator imageInstanceIterator) {
+    this.imageInstanceIterator = imageInstanceIterator;
+  }
 
   @Override
   public String globalInfo() {
@@ -96,20 +169,36 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
     return true;
   }
 
+  private void loadModel(Instances data) throws Exception {
+    if (!useZooModel) {
+      // First try load from the WEKA binary model file
+      try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(serializedModelFile))) {
+        model = (Dl4jMlpClassifier) ois.readObject();
+      } catch (Exception e) {
+        System.err.println("Couldn't load from model file, trying a pretrained zoo model");
+        e.printStackTrace();
+      }
+    } else {
+      // If that fails, try loading from selected zoo model (or keras file)
+      model = new Dl4jMlpClassifier();
+      model.setZooModel(zooModelType);
+      model.setInstanceIterator(imageInstanceIterator);
+      model.initializeClassifier(data);
+
+      transformationLayerName = zooModelType.getFeatureExtractionLayer();
+    }
+  }
+
   @Override
   protected Instances determineOutputFormat(Instances inputFormat) throws Exception {
-    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(modelFile))) {
-      model = (Dl4jMlpClassifier) ois.readObject();
-      return model.getActivationsAtLayer(transformationLayerName, inputFormat);
-    } catch (IOException e) {
-      throw new WekaException("Could not read model file: " + modelFile.getAbsolutePath());
-    }
+    loadModel(inputFormat);
+    return model.getActivationsAtLayer(transformationLayerName, inputFormat, useZooModel);
   }
 
 
   @Override
   protected Instances process(Instances instances) throws Exception {
-    return model.getActivationsAtLayer(transformationLayerName, instances);
+    return model.getActivationsAtLayer(transformationLayerName, instances, useZooModel);
   }
 
 
@@ -119,7 +208,6 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
    * @return an enumeration of all the available options.
    */
   public Enumeration<Option> listOptions() {
-
     return Option.listOptionsForClassHierarchy(this.getClass(), Filter.class).elements();
   }
 
@@ -129,7 +217,6 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
    * @return an array of strings suitable for passing to setOptions
    */
   public String[] getOptions() {
-
     return Option.getOptionsForHierarchy(this, Filter.class);
   }
 
@@ -140,7 +227,6 @@ public class Dl4jMlpFilter extends SimpleBatchFilter implements OptionHandler {
    * @throws Exception if an option is not supported
    */
   public void setOptions(String[] options) throws Exception {
-
     Option.setOptionsForHierarchy(options, this, Filter.class);
   }
 }
