68076c25010ebd2d45f35f8a0fc45aa4bd6ac119
Rhys <rhys.compton@gmail.com>
2020-03-18 14:12:09 +1300

    Added NASNet - pretrained not working

---
+++ src/main/java/weka/dl4j/zoo/NASNet.java
@@ -0,0 +1,67 @@
+/*
+ * WekaDeeplearning4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WekaDeeplearning4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WekaDeeplearning4j.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ResNet50.java
+ * Copyright (C) 2017-2018 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.dl4j.zoo;
+
+import org.deeplearning4j.nn.conf.CacheMode;
+import org.deeplearning4j.nn.conf.ComputationGraphConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.transferlearning.TransferLearning;
+import org.deeplearning4j.zoo.PretrainedType;
+import org.deeplearning4j.zoo.ZooModel;
+import weka.dl4j.Preferences;
+
+/**
+ * A WEKA version of DeepLearning4j's ResNet50 ZooModel.
+ *
+ * @author Steven Lang
+ */
+public class NASNet extends AbstractZooModel {
+
+    // TODO get pretrained working
+
+    private static final long serialVersionUID = -5206943146661L;
+
+    @Override
+    public NASNet setPretrainedType(PretrainedType pretrainedType) {
+        return (NASNet) setPretrainedType(pretrainedType, 2048, "fc1000", "flatten_1");
+    }
+
+    @Override
+    public ComputationGraph init(int numLabels, long seed, int[] shape) {
+        org.deeplearning4j.zoo.model.NASNet net = org.deeplearning4j.zoo.model.NASNet.builder()
+                .cacheMode(CacheMode.NONE)
+                .workspaceMode(Preferences.WORKSPACE_MODE)
+                .inputShape(shape)
+                .numClasses(numLabels)
+                .build();
+
+        // DL4J bug? Throws IllegalArgumentException: Cannot calculate activation types if no inputs have been set (use addInputs(String...))
+        // However addInputs() is not available on NASNet.builder()
+        ComputationGraph defaultNet = net.init();
+
+        return attemptToLoadWeights(net, defaultNet, seed, numLabels);
+    }
+
+
+    @Override
+    public int[][] getShape() {
+        return org.deeplearning4j.zoo.model.ResNet50.builder().build().metaData().getInputShape();
+    }
+}
