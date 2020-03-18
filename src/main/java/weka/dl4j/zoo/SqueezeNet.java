bb088bb6ac6183f682d9eb2ab251a80bb7f69cc9
Rhys <rhys.compton@gmail.com>
2020-03-18 14:50:12 +1300

    Added SqueezeNet model - pretrained not working

---
+++ src/main/java/weka/dl4j/zoo/SqueezeNet.java
@@ -0,0 +1,69 @@
+/*
+ * WekaDeeplearning4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WekaDeeplearning4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WekaDeeplearning4j.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ResNet50.java
+ * Copyright (C) 2017-2018 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.dl4j.zoo;
+
+import org.deeplearning4j.nn.conf.CacheMode;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.zoo.PretrainedType;
+import weka.dl4j.Preferences;
+
+/**
+ * A WEKA version of DeepLearning4j's ResNet50 ZooModel.
+ *
+ * @author Steven Lang
+ */
+public class SqueezeNet extends AbstractZooModel {
+
+    // TODO get pretrained working
+
+    private static final long serialVersionUID = -5206943146661L;
+
+    @Override
+    public SqueezeNet setPretrainedType(PretrainedType pretrainedType) {
+        return (SqueezeNet) setPretrainedType(pretrainedType,
+                512,
+                "drop9",
+                "conv10",
+                new String[] {
+                        "relu10",
+                        "global_average_pooling2d_5",
+                        "loss"
+                });
+    }
+
+    @Override
+    public ComputationGraph init(int numLabels, long seed, int[] shape) {
+        org.deeplearning4j.zoo.model.SqueezeNet net = org.deeplearning4j.zoo.model.SqueezeNet.builder()
+                .cacheMode(CacheMode.NONE)
+                .workspaceMode(Preferences.WORKSPACE_MODE)
+                .inputShape(shape)
+                .numClasses(numLabels)
+                .build();
+
+        ComputationGraph defaultNet = net.init();
+
+        return attemptToLoadWeights(net, defaultNet, seed, numLabels);
+    }
+
+
+    @Override
+    public int[][] getShape() {
+        return org.deeplearning4j.zoo.model.ResNet50.builder().build().metaData().getInputShape();
+    }
+}
