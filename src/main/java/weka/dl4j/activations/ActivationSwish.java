de156942326d20798d39f175b3f07ce3bc8be019
Rhys <rhys.compton@gmail.com>
2020-03-24 13:22:28 +1300

    Added swish activation function (not yet implemented in DL4J, PR made, awaiting acceptance)

---
+++ src/main/java/weka/dl4j/activations/ActivationSwish.java
@@ -0,0 +1,75 @@
+/*
+ * WekaDeeplearning4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WekaDeeplearning4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WekaDeeplearning4j.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ActivationReLU.java
+ * Copyright (C) 2017-2018 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.dl4j.activations;
+
+import org.nd4j.shade.jackson.annotation.JsonTypeName;
+import weka.core.Option;
+import weka.core.OptionHandler;
+
+import java.util.Enumeration;
+
+/**
+ * A version of DeepLearning4j's ActivationSwish that implements WEKA option handling.
+ *
+ * @author Rhys Compton
+ */
+@JsonTypeName("Swish")
+public class ActivationSwish extends Activation<org.nd4j.linalg.activations.impl.ActivationSwish>
+    implements OptionHandler {
+
+  private static final long serialVersionUID = -705527390430410412L;
+
+  @Override
+  public void initializeBackend() {
+    backend = new org.nd4j.linalg.activations.impl.ActivationSwish();
+  }
+
+  /**
+   * Returns an enumeration describing the available options.
+   *
+   * @return an enumeration of all the available options.
+   */
+  @Override
+  public Enumeration<Option> listOptions() {
+
+    return Option.listOptionsForClass(this.getClass()).elements();
+  }
+
+  /**
+   * Gets the current settings of the Classifier.
+   *
+   * @return an array of strings suitable for passing to setOptions
+   */
+  @Override
+  public String[] getOptions() {
+
+    return Option.getOptions(this, this.getClass());
+  }
+
+  /**
+   * Parses a given list of options.
+   *
+   * @param options the list of options as an array of strings
+   * @throws Exception if an option is not supported
+   */
+  public void setOptions(String[] options) throws Exception {
+
+    Option.setOptions(options, this, this.getClass());
+  }
+}
