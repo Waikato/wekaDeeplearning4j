753ff24bad9a6e50b0b1a775dede1311a46450d5
Rhys <rhys.compton@gmail.com>
2020-05-09 00:42:12 +1200

    Remove String[] from GUIEditors

--- src/main/java/myTest/WekaTests.java
+++ src/main/java/myTest/WekaTests.java
@@ -4,6 +4,7 @@ import weka.classifiers.Evaluation;
 import weka.classifiers.functions.Dl4jMlpClassifier;
 import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
 import weka.dl4j.NeuralNetConfiguration;
 import weka.dl4j.PoolingType;
 import weka.dl4j.PretrainedType;
@@ -15,6 +16,7 @@ import weka.dl4j.zoo.Dl4jVGG;
 import weka.dl4j.zoo.keras.DenseNet;
 import weka.dl4j.zoo.keras.NASNet;
 import weka.dl4j.zoo.keras.ResNet;
+import weka.dl4j.zoo.keras.VGG;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Dl4jMlpFilter;
 
@@ -22,40 +24,33 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Random;
 
-class WekaTests {
+public class WekaTests {
     public WekaTests() {}
 
     public void filterExample(String[] args) throws Exception {
-        // Load the dataset
-        Instances instances = new Instances(new FileReader("datasets/nominal/mnist.meta.minimal.arff"));
+// Load all packages so that Dl4jMlpFilter class can be found using forName("weka.filters.unsupervised.attribute.Dl4jMlpFilter")
+        weka.core.WekaPackageManager.loadPackages(true);
+
+// Load the dataset
+        weka.core.Instances instances = new weka.core.Instances(new FileReader("E:\\Rhys\\Documents\\Github\\wekaDeeplearning4j\\datasets\\nominal\\mnist.meta.minimal.arff"));
         instances.setClassIndex(1);
-        Dl4jMlpFilter myFilter = new Dl4jMlpFilter();
-
-        // Load our pretrained model (must be done *before* specifying extra transformation layers)
-        Dl4JResNet50 zooModel = new Dl4JResNet50();
-        myFilter.setZooModelType(zooModel);
-
-        // Concatenate activations from an intermediate convolution layer
-        myFilter.addTransformationLayerName("res4a_branch2b");
-        // Set the pooling type to average
-        myFilter.setPoolingType(PoolingType.AVG);
-
-        // Create our iterator, pointing it to the location of the images
-        ImageInstanceIterator imgIter = new ImageInstanceIterator();
-        imgIter.setImagesLocation(new File("datasets/nominal/mnist-minimal"));
-        // Featurize 16 instances at a time
-        imgIter.setTrainBatchSize(16);
-        myFilter.setImageInstanceIterator(imgIter);
+        String[] filterOptions = weka.core.Utils.splitOptions("-iterator \".ImageInstanceIterator -imagesLocation E:\\\\Rhys\\\\Documents\\\\Github\\\\wekaDeeplearning4j\\\\datasets\\\\nominal\\\\mnist-minimal -bs 12\" -poolingType AVG -layer-extract \"weka.dl4j.layers.DenseLayer -name res4a_branch2b\" -layer-extract \"weka.dl4j.layers.DenseLayer -name flatten_1\" -isZoo true -zooModel \".Dl4JResNet50\"");
+        weka.filters.Filter myFilter = (weka.filters.Filter) weka.core.Utils.forName(weka.filters.Filter.class, "weka.filters.unsupervised.attribute.Dl4jMlpFilter", filterOptions);
+//        myFilter.setOptions();
 
 // Run the filter, using the model as a feature extractor
         myFilter.setInputFormat(instances);
-        Instances transformedInstances = Filter.useFilter(instances, myFilter);
+        weka.core.Instances transformedInstances = weka.filters.Filter.useFilter(instances, myFilter);
+
+// You could save the instances at this point to an arff file to be used with other classifiers via:
+// https://waikato.github.io/weka-wiki/formats_and_processing/save_instances_to_arff/
 
 // CV our Random Forest classifier on the extracted features
-        Evaluation evaluation = new Evaluation(transformedInstances);
+        weka.classifiers.evaluation.Evaluation evaluation = new weka.classifiers.evaluation.Evaluation(transformedInstances);
         int numFolds = 10;
-        evaluation.crossValidateModel(new RandomForest(), transformedInstances, numFolds, new Random(1));
+        evaluation.crossValidateModel(new weka.classifiers.trees.RandomForest(), transformedInstances, numFolds, new Random(1));
         System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.toMatrixString());
     }
 
     public void filterTest(String[] args) throws Exception {
@@ -64,8 +59,9 @@ class WekaTests {
         imgIter.setImagesLocation(new File("datasets/nominal/mnist-minimal"));
         imgIter.setTrainBatchSize(16);
         myFilter.setImageInstanceIterator(imgIter);
-        Dl4JResNet50 zooModel = new Dl4JResNet50();
-        myFilter.addTransformationLayerName("res4a_branch2b");
+        Dl4jSimpleCNN zooModel = new Dl4jSimpleCNN();
+//        zooModel.setVariation(NASNet.VARIATION.MOBILE);
+//        myFilter.addTransformationLayerName("reduction_conv_1_stem_1");
         myFilter.setZooModelType(zooModel);
         Filter.runFilter(myFilter, args);
     }
@@ -73,14 +69,14 @@ class WekaTests {
     public void train(String[] args) throws Exception {
         Dl4jMlpClassifier clf = new Dl4jMlpClassifier();
         clf.setSeed(1);
-        clf.setNumEpochs(2);
+        clf.setNumEpochs(0);
 
         // Load the arff file
-        Instances data = new Instances(new FileReader("E:\\Rhys\\Documents\\Github\\kaggle-competitions\\plant-seedlings\\data\\train\\plant-seedlings-train.arff"));
+        Instances data = new Instances(new FileReader("datasets\\nominal\\mnist.meta.minimal.arff"));
         data.setClassIndex(data.numAttributes() - 1);
 
         ImageInstanceIterator imgIter = new ImageInstanceIterator();
-        imgIter.setImagesLocation(new File("E:\\Rhys\\Documents\\Github\\kaggle-competitions\\plant-seedlings\\data\\train"));
+        imgIter.setImagesLocation(new File("datasets\\nominal\\mnist-minimal"));
         imgIter.setTrainBatchSize(16);
         clf.setInstanceIterator(imgIter);
 
@@ -92,8 +88,8 @@ class WekaTests {
         clf.setNeuralNetConfiguration(nnc);
 
         // Set up the pretrained model
-        KerasResNet zooModel = new KerasResNet();
-        zooModel.setVariation(ResNet.VARIATION.RESNET152V2);
+        Dl4jAlexNet zooModel = new Dl4jAlexNet();
+//        zooModel.setVariation(Dl4jDarknet19.VARIATION.INPUT448);
         clf.setZooModel(zooModel);
 
         // Stratify and split the data
@@ -115,9 +111,7 @@ class WekaTests {
         System.out.println(eval.toSummaryString());
         System.out.println(eval.toMatrixString());
     }
-}
 
-public class Main {
     public static void main(String[] args) throws Exception {
         new WekaTests().filterTest(args);
     }
