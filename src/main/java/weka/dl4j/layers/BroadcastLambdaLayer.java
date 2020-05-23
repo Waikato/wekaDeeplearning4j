726f295fa01ba24df3d651192b909b02d141e106
Rhys <rhys.compton@gmail.com>
2020-05-23 22:26:58 +1200

    Add WEKA option handler class for CustomBroadcast

---
+++ src/main/java/weka/dl4j/layers/BroadcastLambdaLayer.java
@@ -0,0 +1,106 @@
+/*
+ * WekaDeeplearning4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WekaDeeplearning4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WekaDeeplearning4j.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ConvolutionLayer.java
+ * Copyright (C) 2016-2018 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.dl4j.layers;
+
+import weka.core.Option;
+import weka.core.OptionHandler;
+import weka.core.OptionMetadata;
+import weka.dl4j.AlgoMode;
+import weka.dl4j.ConvolutionMode;
+import weka.dl4j.layers.lambda.CustomBroadcast;
+import weka.gui.ProgrammaticProperty;
+
+import java.io.Serializable;
+import java.util.Enumeration;
+
+/**
+ * A WEKA option handler class for the CustomBroadcast layer, required to use EfficientNet.
+ *
+ * @author Rhys Compton
+ */
+public class BroadcastLambdaLayer extends SameDiffLayer<CustomBroadcast>
+    implements OptionHandler, Serializable {
+
+  /**
+   * The ID used to serialize this class.
+   */
+  private static final long serialVersionUID = 6905344091980568487L;
+
+  /**
+   * Constructor for setting some defaults.
+   */
+  public BroadcastLambdaLayer() {
+    super();
+    setLayerName("Custom EfficientNet Broadcast layer");
+  }
+
+  @Override
+  public void initializeBackend() {
+    backend = new CustomBroadcast(-1);
+  }
+
+  @Override
+  public CustomBroadcast getBackend() {
+    return backend;
+  }
+
+  @Override
+  public void setBackend(CustomBroadcast newBackend) {
+    this.backend = newBackend;
+  }
+
+  /**
+   * Global info.
+   *
+   * @return string describing this class.
+   */
+  public String globalInfo() {
+    return "A convolution layer from DeepLearning4J.";
+  }
+
+  /**
+   * Returns an enumeration describing the available options.
+   *
+   * @return an enumeration of all the available options.
+   */
+  @Override
+  public Enumeration<Option> listOptions() {
+    return Option.listOptionsForClassHierarchy(this.getClass(), super.getClass()).elements();
+  }
+
+  /**
+   * Gets the current settings of the Classifier.
+   *
+   * @return an array of strings suitable for passing to setOptions
+   */
+  @Override
+  public String[] getOptions() {
+    return Option.getOptionsForHierarchy(this, super.getClass());
+  }
+
+  /**
+   * Parses a given list of options.
+   *
+   * @param options the list of options as an array of strings
+   * @throws Exception if an option is not supported
+   */
+  public void setOptions(String[] options) throws Exception {
+    Option.setOptionsForHierarchy(options, this, super.getClass());
+  }
+}
