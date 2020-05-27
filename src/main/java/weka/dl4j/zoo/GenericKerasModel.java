0279dcc22dea3014a3d4c4a0b174c51e45c446a1
Rhys <rhys.compton@gmail.com>
2020-05-27 16:37:19 +1200

    Hide GenericKerasModel
    Keras loading isn't working currently, so this class is too unreliable

--- src/main/java/weka/dl4j/zoo/GenericKerasModel.java
+++ src/main/java/weka/dl4j/zoo/GenericKerasModel.java
@@ -35,43 +35,45 @@ import java.io.File;
  *
  * @author Rhys Compton
  */
-public class GenericKerasModel extends AbstractZooModel {
-
-  private static final long serialVersionUID = 7572785977498659081L;
-
-  protected File kerasH5File = new File(WekaPackageManager.getPackageHome().toURI());
-
-  @Override
-  public ComputationGraph init(int numLabels, long seed, int[] shape, boolean filterMode) {
-    try {
-      ComputationGraph computationGraph = KerasModelImport.importKerasModelAndWeights(kerasH5File.getPath());
-
-      return addFinalOutputLayer(computationGraph, seed, numLabels);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      return null;
-    }
-  }
-
-  @OptionMetadata(
-          description = "The trained Keras model file.",
-          displayName = "Keras H5 file",
-          commandLineParamName = "model",
-          commandLineParamSynopsis = "-model <File>",
-          displayOrder = 1
-  )
-  public File getKerasH5File() {
-    return kerasH5File;
-  }
-
-  public void setKerasH5File(File modelPath) {
-    this.kerasH5File = modelPath;
-  }
-
-  @Override
-  public int[][] getShape() {
-    int[][] shape = new int[1][];
-    shape[0] = new int[] {3, 224, 224};
-    return shape;
-  }
+public class GenericKerasModel {
+  // Keras model importing is broken at the moment (27/05/2020) - will implement this when working more reliably
+//public class GenericKerasModel extends AbstractZooModel {
+//
+//  private static final long serialVersionUID = 7572785977498659081L;
+//
+//  protected File kerasH5File = new File(WekaPackageManager.getPackageHome().toURI());
+//
+//  @Override
+//  public ComputationGraph init(int numLabels, long seed, int[] shape, boolean filterMode) {
+//    try {
+//      ComputationGraph computationGraph = KerasModelImport.importKerasModelAndWeights(kerasH5File.getPath());
+//
+//      return addFinalOutputLayer(computationGraph, seed, numLabels);
+//    } catch (Exception ex) {
+//      ex.printStackTrace();
+//      return null;
+//    }
+//  }
+//
+//  @OptionMetadata(
+//          description = "The trained Keras model file.",
+//          displayName = "Keras H5 file",
+//          commandLineParamName = "model",
+//          commandLineParamSynopsis = "-model <File>",
+//          displayOrder = 1
+//  )
+//  public File getKerasH5File() {
+//    return kerasH5File;
+//  }
+//
+//  public void setKerasH5File(File modelPath) {
+//    this.kerasH5File = modelPath;
+//  }
+//
+//  @Override
+//  public int[][] getShape() {
+//    int[][] shape = new int[1][];
+//    shape[0] = new int[] {3, 224, 224};
+//    return shape;
+//  }
 }
