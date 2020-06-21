d5a06b140fea37d6e5ef77de9d76c0936bf184f4
Rhys <rhys.compton@gmail.com>
2020-06-21 16:45:44 +1200

    Clean up update-javadocs

--- update-javadocs.sh
+++ update-javadocs.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -x
 
+TMP_FOLDER="../temp_jdoc"
+
+# Build the docs
 ./gradlew clean javaDoc
-mkdir ../temp_jdoc
-cp -r build/docs/javadoc/* ../temp_jdoc
+# Folder to copy docs into
+mkdir $TMP_FOLDER
+# Copy them out of the repo
+cp -r build/docs/javadoc/* $TMP_FOLDER
+# Switch to correct branch
 git checkout gh-pages
+# Replace old docs with newly built ones
 rm -rf *
-cp -r ../temp_jdoc/* .
+cp -r TMP_FOLDER .
+# Commit to the gh-pages branch
 git add .
 git commit -m 'Update javadoc'
 git push
-git checkout master
 
-rm -rf ../temp_jdoc
+# Clean up
+git checkout master
+rm -rf $TMP_FOLDER
 
 echo "JavaDocs update was successful!"
