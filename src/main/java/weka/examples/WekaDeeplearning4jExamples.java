daed600aa719c8e3ab3920ae5cadf12c74919484
Rhys <rhys.compton@gmail.com>
2020-06-30 14:01:47 +1200

    Package usage example does proper evaluation now

--- src/main/java/weka/examples/WekaDeeplearning4jExamples.java
+++ src/main/java/weka/examples/WekaDeeplearning4jExamples.java
@@ -1,5 +1,6 @@
 package weka.examples;
 
+import weka.classifiers.Evaluation;
 import weka.classifiers.functions.Dl4jMlpClassifier;
 import weka.core.Instances;
 import weka.core.converters.ImageDirectoryLoader;
@@ -8,6 +9,7 @@ import weka.dl4j.zoo.KerasEfficientNet;
 import weka.dl4j.zoo.keras.EfficientNet;
 
 import java.io.File;
+import java.util.Random;
 
 public class WekaDeeplearning4jExamples {
 
@@ -34,6 +36,22 @@ public class WekaDeeplearning4jExamples {
 
         classifier.setInstanceIterator(iterator);
 
-        classifier.buildClassifier(inst);
+        // Stratify and split the data
+        Random rand = new Random(0);
+        inst.randomize(rand);
+        inst.stratify(5);
+        Instances train = inst.trainCV(5, 0);
+        Instances test = inst.testCV(5, 0);
+
+// Build the classifier on the training data
+        classifier.buildClassifier(train);
+
+// Evaluate the model on test data
+        Evaluation eval = new Evaluation(test);
+        eval.evaluateModel(classifier, test);
+
+// Output some summary statistics
+        System.out.println(eval.toSummaryString());
+        System.out.println(eval.toMatrixString());
     }
 }
