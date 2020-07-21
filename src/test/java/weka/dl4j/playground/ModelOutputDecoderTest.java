907f1ad3b1e2d3d54be40acc46756e4b10765e7d
Rhys <rhys.compton@gmail.com>
2020-07-22 11:08:22 +1200

    :white_check_mark: Update ModelOutputDecoderTest

--- src/test/java/weka/dl4j/playground/ModelOutputDecoderTest.java
+++ src/test/java/weka/dl4j/playground/ModelOutputDecoderTest.java
@@ -1,7 +1,6 @@
 package weka.dl4j.playground;
 
 import junit.framework.TestCase;
-import org.junit.Assert;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import weka.util.DatasetLoader;
 
@@ -10,25 +9,17 @@ public class ModelOutputDecoderTest extends TestCase {
     public void testDecodePredictions() throws Exception {
         INDArray modelPredictions = DatasetLoader.loadCarPredictions();
 
-        ModelOutputDecoder decoder = new ModelOutputDecoder(new ClassMap(ClassMap.BuiltInClassMap.IMAGENET));
+        ModelOutputDecoder decoder = new ModelOutputDecoder();
+        decoder.setBuiltInClassMap(ModelOutputDecoder.ClassmapType.IMAGENET);
 
-        TopNPredictions[] decoded = decoder.decodePredictions(modelPredictions);
+        TopNPredictions carPrediction = decoder.decodePredictions(modelPredictions);
 
-        assertNotNull(decoded);
+        assertNotNull(carPrediction);
 
-        // Test there's only one in the batch
-        assertEquals(decoded.length, 1);
-
-
-        TopNPredictions carPrediction = decoded[0];
-
-        // Get the highest probability prediction
         Prediction highestProbPrediction = carPrediction.getTopPrediction();
 
         assertEquals(817, highestProbPrediction.getClassID());
-
         assertEquals("sports car, sport car", highestProbPrediction.getClassName());
-
         assertEquals(0.767, highestProbPrediction.getClassProbability(), 0.001);
     }
 }
