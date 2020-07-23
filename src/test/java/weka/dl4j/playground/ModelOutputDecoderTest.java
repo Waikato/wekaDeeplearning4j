9a9b8319dd2f035ceb79a885b3b1e68b0bd98679
Rhys <rhys.compton@gmail.com>
2020-07-23 14:01:02 +1200

    :white_check_mark: Add new ModelOutputDecoderTest

--- src/test/java/weka/dl4j/playground/ModelOutputDecoderTest.java
+++ src/test/java/weka/dl4j/playground/ModelOutputDecoderTest.java
@@ -2,6 +2,7 @@ package weka.dl4j.playground;
 
 import junit.framework.TestCase;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import weka.core.WekaException;
 import weka.util.DatasetLoader;
 
 /**
@@ -25,5 +26,25 @@ public class ModelOutputDecoderTest extends TestCase {
         assertEquals(817, highestProbPrediction.getClassID());
         assertEquals("sports car, sport car", highestProbPrediction.getClassName());
         assertEquals(0.767, highestProbPrediction.getClassProbability(), 0.001);
+    }
+
+    /**
+     * Tests that the method fails properly when an invalid setup is provided.
+     * Will complain about number of prediction classes not matching size of class map
+     * @throws Exception
+     */
+    public void testIncorrectSetup() throws Exception {
+        INDArray modelPredictions = DatasetLoader.loadCarPredictions();
+
+        ModelOutputDecoder decoder = new ModelOutputDecoder();
+        decoder.setBuiltInClassMap(ModelOutputDecoder.ClassmapType.VGGFACE);
+
+        try {
+            decoder.decodePredictions(modelPredictions);
+        } catch (Exception ex) {
+            assertTrue(ex instanceof WekaException);
+            return;
+        }
+        fail();
     }
 }
