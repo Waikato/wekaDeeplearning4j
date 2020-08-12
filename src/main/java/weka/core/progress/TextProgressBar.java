d805596a50f15bf68771f94fce6f5a7824778b24
Rhys <rhys.compton@gmail.com>
2020-08-13 09:52:27 +1200

    :recycle: Refactor ProgressBar into TextProgressBar
    Will have GUIProgressBar soon

---
+++ src/main/java/weka/core/progress/TextProgressBar.java
@@ -0,0 +1,44 @@
+package weka.core.progress;
+
+import weka.gui.Logger;
+import weka.gui.SysErrLog;
+// TODO document
+public class TextProgressBar extends AbstractProgressBar {
+
+    public TextProgressBar() {}
+
+    public TextProgressBar(double maxProgress) {
+        reset(maxProgress);
+    }
+
+    private String progressChar = "-";
+
+    protected int progressBarSize = 60;
+
+    private int numDots = 0;
+
+    public void setProgress(double progress) {
+        super.setProgress(progress);
+        numDots = (int) (m_normalizedProgress * progressBarSize);
+
+        refreshDisplay();
+    }
+
+    public void refreshDisplay() {
+        System.err.print("\rProgress:  |" + progressChar.repeat(numDots) + " ".repeat(progressBarSize - numDots) + "|");
+
+        if (numDots == progressBarSize) {
+            System.err.println("\nDone!");
+        }
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
