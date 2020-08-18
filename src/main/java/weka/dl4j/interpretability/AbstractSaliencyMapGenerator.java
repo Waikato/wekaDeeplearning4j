cbf083556c927b8de7f45923d7adb8a4882fb372
rhyscompton <rhys.compton@gmail.com>
2020-08-18 15:55:20 +1200

    :sparkles: Add fields to AbstractSaliencyMapGenerator

--- src/main/java/weka/dl4j/interpretability/AbstractSaliencyMapGenerator.java
+++ src/main/java/weka/dl4j/interpretability/AbstractSaliencyMapGenerator.java
@@ -1,18 +1,52 @@
 package weka.dl4j.interpretability;
 
 import org.deeplearning4j.nn.graph.ComputationGraph;
-import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 
 import java.io.File;
 
 // TODO document
 public abstract class AbstractSaliencyMapGenerator {
 
-    protected ComputationGraph model;
+    protected ComputationGraph computationGraph = null;
 
-    public AbstractSaliencyMapGenerator(ComputationGraph model) {
-        this.model = model;
+    protected int targetClassID = -1;
+
+    protected int batchSize = 1;
+
+    protected ImagePreProcessingScaler imagePreProcessingScaler = null;
+
+    public abstract void generateForImage(String inputImagePath, String outputImagePath);
+
+    public ComputationGraph getComputationGraph() {
+        return computationGraph;
     }
 
-    public abstract void generateForImage(File imageFile, int targetClassID);
+    public void setComputationGraph(ComputationGraph computationGraph) {
+        this.computationGraph = computationGraph;
+    }
+
+    public int getTargetClassID() {
+        return targetClassID;
+    }
+
+    public void setTargetClassID(int targetClassID) {
+        this.targetClassID = targetClassID;
+    }
+
+    public int getBatchSize() {
+        return batchSize;
+    }
+
+    public void setBatchSize(int batchSize) {
+        this.batchSize = batchSize;
+    }
+
+    public ImagePreProcessingScaler getImagePreProcessingScaler() {
+        return imagePreProcessingScaler;
+    }
+
+    public void setImagePreProcessingScaler(ImagePreProcessingScaler imagePreProcessingScaler) {
+        this.imagePreProcessingScaler = imagePreProcessingScaler;
+    }
 }
