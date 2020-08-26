b871969a33caa000089aa0d981c084508af0b7b5
Rhys <rhys.compton@gmail.com>
2020-08-26 12:21:04 +1200

    :white_check_mark: Add ScoreCAMTest

---
+++ src/test/java/weka/dl4j/interpretability/ScoreCAMTest.java
@@ -0,0 +1,46 @@
+package weka.dl4j.interpretability;
+
+import junit.framework.TestCase;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import weka.classifiers.functions.dl4j.Utils;
+import weka.dl4j.zoo.AbstractZooModel;
+import weka.dl4j.zoo.KerasResNet;
+import weka.dl4j.zoo.keras.ResNet;
+import weka.zoo.ZooModelTest;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.List;
+
+public class ScoreCAMTest extends TestCase {
+
+    public void testAllKerasModels() {
+        List<AbstractZooModel> kerasZooModels = ZooModelTest.createKerasModels();
+
+        boolean tmpDir = new File("tmp").mkdir();
+
+        for (AbstractZooModel pretrainedModel : kerasZooModels) {
+            ComputationGraph computationGraph = pretrainedModel.getDefaultGraph();
+
+            ScoreCAM scoreCAM = new ScoreCAM();
+            scoreCAM.setBatchSize(8);
+            scoreCAM.setComputationGraph(computationGraph);
+            scoreCAM.setModelInputShape(Utils.decodeCNNShape(pretrainedModel.getShape()[0]));
+            scoreCAM.setImageChannelsLast(pretrainedModel.getChannelsLast());
+            scoreCAM.setImagePreProcessingScaler(pretrainedModel.getImagePreprocessingScaler());
+            scoreCAM.generateForImage("src/test/resources/images/dog.jpg");
+
+            String modelName = pretrainedModel.getPrettyName();
+
+            try {
+                ImageIO.write(scoreCAM.getOriginalImage(), "png", new File(String.format("tmp/%s_original.png", modelName)));
+                ImageIO.write(scoreCAM.getHeatmap(), "png", new File(String.format("tmp/%s_heatmap.png", modelName)));
+                ImageIO.write(scoreCAM.getHeatmapOnImage(), "png", new File(String.format("tmp/%s_heatmapOnImage.png", modelName)));
+            } catch (OutOfMemoryError error) {
+                error.printStackTrace();
+            } catch (Exception ex) {
+                fail();
+            }
+        }
+    }
+}
