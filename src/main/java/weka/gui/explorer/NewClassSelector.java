6627a5e440b838a48ba433ba9bac7570f92b1c0f
Rhys <rhys.compton@gmail.com>
2020-11-01 14:21:23 +1300

    :sparkles: Refactored Saliency map window allows adding multiple class selectors

---
+++ src/main/java/weka/gui/explorer/NewClassSelector.java
@@ -0,0 +1,181 @@
+package weka.gui.explorer;
+
+import weka.dl4j.inference.PredictionClass;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class NewClassSelector {
+
+    private GridBagConstraints gbc;
+
+//    private JPanel parentPanel;
+
+    private int rowNum;
+
+    private String[] classMap;
+
+    String m_PatternRegEx = "";
+
+    private NewSaliencyMapWindow parentPanel;
+
+    JLabel targetClassIDLabel = new JLabel("Target Class ID:");
+    JTextField targetClassIDInput = new JTextField(3);
+    JLabel classNameLabel = new JLabel("  Class Name:");
+    JTextField classNameInput = new JTextField();
+    JButton patternButton = new JButton("Pattern");
+
+//    public NewClassSelector(JPanel parentPanel, GridBagConstraints gbc, String[] classMap, int rowNum, int targetClassID) {
+    public NewClassSelector(NewSaliencyMapWindow parentPanel, int rowNum) {
+        this.parentPanel = parentPanel;
+        this.gbc = parentPanel.gbc;
+        this.classMap = parentPanel.getCurrentClassMap();
+        this.rowNum = rowNum;
+        setTargetClass(parentPanel.getDefaultClassID());
+        setup();
+    }
+
+    private void setup() {
+        patternButton.addActionListener(e -> openPatternDialog());
+
+        // Setup the button listeners
+        targetClassIDInput.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateClassNameInput();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateClassNameInput();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateClassNameInput();
+            }
+        });
+
+        // Define the UI elements
+        targetClassIDInput.setColumns(5);
+        targetClassIDInput.setToolTipText("-1 to use max probability class");
+        classNameInput.setColumns(40);
+        classNameInput.setEditable(false);
+
+        gbc.gridy = rowNum;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+
+        gbc.gridx = 0;
+        parentPanel.add(targetClassIDLabel, gbc);
+
+        gbc.gridx = 1;
+        targetClassIDInput.setMinimumSize(new Dimension(50, 28));
+        parentPanel.add(targetClassIDInput, gbc);
+
+        gbc.gridx = 2;
+        parentPanel.add(classNameLabel, gbc);
+
+        gbc.gridx = 3;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        parentPanel.add(classNameInput, gbc);
+
+        gbc.gridx = 4;
+        gbc.weightx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        parentPanel.add(patternButton, gbc);
+    }
+
+    public void removeFromParent() {
+        parentPanel.remove(targetClassIDLabel);
+        parentPanel.remove(targetClassIDInput);
+        parentPanel.remove(classNameLabel);
+        parentPanel.remove(classNameInput);
+        parentPanel.remove(patternButton);
+    }
+
+    public void setTargetClass(int id) {
+        targetClassIDInput.setText("" + id);
+    }
+
+    public int getTargetClass() {
+        return Integer.parseInt(targetClassIDInput.getText());
+    }
+
+    private void openPatternDialog() {
+        String pattern = JOptionPane.showInputDialog(patternButton.getParent(),
+                "Enter a Perl regular expression", m_PatternRegEx);
+        if (pattern != null) {
+            try {
+                Pattern.compile(pattern);
+                m_PatternRegEx = pattern;
+                ArrayList<PredictionClass> matchingClasses = getMatchingClasses(pattern);
+
+                if (matchingClasses.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "No classes matched that regex pattern");
+                    return;
+                } else if (matchingClasses.size() == 1) {
+                    setTargetClass(matchingClasses.get(0).getClassID());
+                } else {
+                    PredictionClass selectedClass = selectOneOfNClasses(matchingClasses);
+                    setTargetClass(selectedClass.getClassID());
+                }
+
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(patternButton.getParent(), "'" + pattern
+                                + "' is not a valid Perl regular expression!\n" + "Error: " + ex,
+                        "Error in Pattern...", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private PredictionClass selectOneOfNClasses(ArrayList<PredictionClass> matchingClasses) {
+        return (PredictionClass) JOptionPane.showInputDialog(
+                null,
+                "The pattern matched multiple classes, please select one",
+                "Select a class",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                matchingClasses.toArray(), // Array of choices
+                matchingClasses.get(0));
+    }
+
+    private ArrayList<PredictionClass> getMatchingClasses(String pattern) {
+        ArrayList<PredictionClass> result = new ArrayList<>();
+        for (int i = 0; i < classMap.length; i++) {
+            String tmpClass = classMap[i];
+            if (Pattern.matches(pattern, tmpClass)) {
+                result.add(new PredictionClass(i, tmpClass));
+            }
+        }
+        return result;
+    }
+
+    private void updateClassNameInput() {
+        String targetClassIDText = targetClassIDInput.getText();
+        if (targetClassIDText.isEmpty()) {
+            return;
+        }
+        if (this.classMap.length == 0) {
+            return;
+        }
+        int classID;
+        try {
+            classID = Integer.parseInt(targetClassIDText);
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: Please enter a valid integer value", "Error Message",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        String newClassName = classMap[classID];
+        classNameInput.setText(newClassName);
+    }
+}
--- src/main/java/weka/gui/explorer/NewSaliencyMapWindow.java
+++ src/main/java/weka/gui/explorer/NewSaliencyMapWindow.java
@@ -31,14 +31,7 @@ public class NewSaliencyMapWindow extends JPanel {
 
     GridBagConstraints gbc = new GridBagConstraints();
 
-    JLabel targetClassIDLabel = new JLabel("Target Class ID:");
-    JTextField targetClassIDInput = new JTextField(3);
-    JLabel classNameLabel = new JLabel("  Class Name:");
-    JTextField classNameInput = new JTextField();
-    JButton patternButton = new JButton("Pattern");
-
     JLabel saliencyImageLabel = new JLabel();
-    JScrollPane scrollPane;
     Image saliencyImage;
     JCheckBox normalizeHeatmapCheckbox = new JCheckBox("Normalize heatmap");
     JButton addClassButton = new JButton("Add Class");
@@ -51,25 +44,28 @@ public class NewSaliencyMapWindow extends JPanel {
 
     protected FileFilter m_ImageFilter = new ExtensionFileFilter(ExplorerDl4jInference.IMAGE_FILE_EXTENSIONS, "Image files");
 
-    /** The file chooser for selecting model files. */
+    /** The file chooser for saving the image. */
     protected WekaFileChooser m_FileChooser = new WekaFileChooser(new File(System.getProperty("user.dir")));
 
-    JPanel classSelectorPanel;
-    ArrayList<ClassSelector> classSelectors = new ArrayList<>();
+    ArrayList<NewClassSelector> classSelectors = new ArrayList<>();
+
+    int buttonsRow = 0;
+    int targetClassRow = 1;
+    int numTargetClasses = 0;
+    int imageRow = 20;
 
     public NewSaliencyMapWindow() {
-        setup();
+        oneTimeSetup();
     }
 
     private void addClassSelector() {
-        if (classSelectors.size() == 10) {
+        if (classSelectors.size() == 5) {
             // Limit the size to 10
             return;
         }
-        ClassSelector classSelector = new ClassSelector(getCurrentClassMap(), getDefaultClassID());
+        var classSelector = new NewClassSelector(this, targetClassRow + numTargetClasses++);
         classSelectors.add(classSelector);
-        classSelectorPanel.add(classSelector);
-        thisWindow.pack();
+        packWindow();
     }
 
     private void removeClassSelector() {
@@ -77,52 +73,30 @@ public class NewSaliencyMapWindow extends JPanel {
             // Don't go below one class selector
             return;
         }
-        ClassSelector lastClassSelector = classSelectors.get(classSelectors.size() - 1);
-        classSelectorPanel.remove(lastClassSelector);
+        NewClassSelector lastClassSelector = classSelectors.get(classSelectors.size() - 1);
+        lastClassSelector.removeFromParent();
         classSelectors.remove(lastClassSelector);
+        packWindow();
+    }
+
+    private void packWindow() {
+//        var originalDimension = thisWindow.getSize();
         thisWindow.pack();
+//        thisWindow.setSize(originalDimension);
     }
 
-    private void clearClassSelectors() {
-        classSelectors.clear();
-        classSelectorPanel.removeAll();
+    private void setupButtonListeners() {
+        generateButton.addActionListener(e -> generateSaliencyMap());
+        saveHeatmapButton.addActionListener(e -> saveHeatmap());
+        normalizeHeatmapCheckbox.addActionListener(e -> generateSaliencyMap());
+        addClassButton.addActionListener(e -> addClassSelector());
+        removeClassButton.addActionListener(e -> removeClassSelector());
     }
 
-    private void setup() {
-        setLayout(new GridBagLayout());
-        setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createTitledBorder("Saliency Map Viewer"),
-                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-
-        gbc.insets = new Insets(5, 5, 5, 5);
-        
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        add(targetClassIDLabel, gbc);
-
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        targetClassIDInput.setMinimumSize(new Dimension(50, 28));
-        add(targetClassIDInput, gbc);
-
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        add(classNameLabel, gbc);
-
-        gbc.gridx = 3;
-        gbc.gridy = 0;
-        gbc.weightx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        add(classNameInput, gbc);
-
-        gbc.weightx = 0;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.gridx = 4;
-        gbc.gridy = 0;
-        add(patternButton, gbc);
+    private void addControlButtons() {
+        gbc.gridy = buttonsRow;
 
         gbc.gridx = 0;
-        gbc.gridy = 1;
         gbc.gridwidth = 5;
         var buttonPanel = new JPanel(new GridLayout(1, 5, 30, 5));
         buttonPanel.add(addClassButton);
@@ -131,97 +105,37 @@ public class NewSaliencyMapWindow extends JPanel {
         buttonPanel.add(generateButton);
         buttonPanel.add(saveHeatmapButton);
         add(buttonPanel, gbc);
+    }
 
+    private void addScrollableImage() {
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = imageRow;
         gbc.weighty = 1;
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridwidth = 5;
         gbc.gridheight = 1;
         var imageLabel = new JLabel(new ImageIcon(DEFAULT_SALIENCY_IMAGE_PATH));
         var scrollPane = new JScrollPane(imageLabel);
-//        var panel = new JPanel();
-//        panel.setBackground(Color.green);
         add(scrollPane, gbc);
-
-//        gbc.weightx = 1;
-//        gbc.fill = GridBagConstraints.HORIZONTAL;
-//        gbc.gridx = 0;
-//        gbc.gridy = 1;
-//        add(addClassButton, gbc);
-//
-//        gbc.gridx = 1;
-//        gbc.gridy = 1;
-//        add(removeClassButton, gbc);
-//
-//        gbc.gridx = 2;
-//        gbc.gridy = 1;
-//        add(normalizeHeatmapCheckbox, gbc);
-//
-//        gbc.gridx = 3;
-//        gbc.gridy = 1;
-//        add(generateButton, gbc);
-//
-//        gbc.gridx = 4;
-//        gbc.gridy = 1;
-//        add(saveHeatmapButton, gbc);
-//        // Saliency Map Window
-//        generateButton.addActionListener(e -> generateSaliencyMap());
-//        saveHeatmapButton.addActionListener(e -> saveHeatmap());
-//        normalizeHeatmapCheckbox.addActionListener(e -> generateSaliencyMap());
-//        addClassButton.addActionListener(e -> addClassSelector());
-//        removeClassButton.addActionListener(e -> removeClassSelector());
-//
-//        // Panel to define the layout. We are using GridBagLayout
-//        JPanel mainPanel = new JPanel();
-//        GridBagLayout mainLayout = new GridBagLayout();
-//        mainPanel.setLayout(mainLayout);
-//        mainPanel.setBorder(BorderFactory.createCompoundBorder(
-//                BorderFactory.createTitledBorder("Saliency Map Viewer"),
-//                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
-//
-//        // Setup top row in saliency window - class selector UI
-//        classSelectorPanel = new JPanel();
-//        BoxLayout classSelectorLayout = new BoxLayout(classSelectorPanel, BoxLayout.Y_AXIS);
-//        classSelectorPanel.setLayout(classSelectorLayout);
-//
-//        GridBagConstraints gbC = new GridBagConstraints();
-//        gbC.anchor = GridBagConstraints.CENTER;
-//        gbC.gridx = 0;
-//        gbC.gridy = 0;
-//        mainLayout.setConstraints(classSelectorPanel, gbC);
-//        mainPanel.add(classSelectorPanel);
-//
-//        // Setup second row - Normalize checkbox and Generate button
-//        JPanel secondRow = new JPanel(new GridLayout(1, 5, 30, 5));
-//        secondRow.add(addClassButton);
-//        secondRow.add(removeClassButton);
-//        secondRow.add(normalizeHeatmapCheckbox);
-//        secondRow.add(generateButton);
-//        secondRow.add(saveHeatmapButton);
-//        gbC = new GridBagConstraints();
-//        gbC.anchor = GridBagConstraints.CENTER;
-//        gbC.gridx = 0;
-//        gbC.gridy = 1;
-//        gbC.insets = new Insets(5, 0, 20, 0);
-//        mainLayout.setConstraints(secondRow, gbC);
-//        mainPanel.add(secondRow);
-//
-//        gbC = new GridBagConstraints();
-//        gbC.anchor = GridBagConstraints.CENTER;
-//        gbC.gridx = 0;
-//        gbC.gridy = 2;
-//        scrollPane = new JScrollPane(saliencyImageLabel);
-//        scrollPane.getVerticalScrollBar().setUnitIncrement(20);
-//        scrollPane.setPreferredSize(new Dimension(800, 400));
-//        mainLayout.setConstraints(scrollPane, gbC);
-//        mainPanel.add(scrollPane);
-//
-//        // Add panel to frame
-//        thisWindow.add(mainPanel);
     }
 
-    private String[] getCurrentClassMap() {
+    private void oneTimeSetup() {
+        setupButtonListeners();
+
+        setLayout(new GridBagLayout());
+        setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder("Saliency Map Viewer"),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+
+
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        addControlButtons();
+
+        addScrollableImage();
+    }
+
+    public String[] getCurrentClassMap() {
         if (processedExplorer == null) {
             return new String[0];
         } else {
@@ -230,38 +144,28 @@ public class NewSaliencyMapWindow extends JPanel {
     }
 
     public void open(Dl4jCNNExplorer explorer) {
-        var window = new NewSaliencyMapWindow();
-        var jf = new JFrame();
+        this.processedExplorer = explorer;
 
-        jf.setTitle("WekaDeeplearning4j - Saliency Map Viewer");
-        jf.setSize(1000, 700);
-        jf.setLocationRelativeTo(null);
-        jf.setVisible(true);
-        jf.add(window);
+        // Reset the image
+        try {
+            setSaliencyImage(ImageIO.read(new File(DEFAULT_SALIENCY_IMAGE_PATH)));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-        System.err.println(classNameInput.getHeight());
-//        this.processedExplorer = explorer;
-//
-//        // Reset the image
-//        try {
-//            setSaliencyImage(ImageIO.read(new File(DEFAULT_SALIENCY_IMAGE_PATH)));
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//
-//        // Start with a fresh class selector
-//        clearClassSelectors();
-//        addClassSelector();
-//
-//        // Set the default class ID in the window
-//        normalizeHeatmapCheckbox.setSelected(true);
-//
-//        thisWindow.pack();
-//        thisWindow.setLocationRelativeTo(null);
-//        thisWindow.setVisible(true);
+        // Start with a fresh class selector
+        addClassSelector();
+
+        // Set the default class ID in the window
+        normalizeHeatmapCheckbox.setSelected(true);
+
+        thisWindow.add(this);
+        packWindow();
+        thisWindow.setLocationRelativeTo(null);
+        thisWindow.setVisible(true);
     }
 
-    private int getDefaultClassID() {
+    public int getDefaultClassID() {
         if (processedExplorer != null)
             return processedExplorer.getCurrentPredictions().getTopPrediction().getClassID();
         else
@@ -269,7 +173,7 @@ public class NewSaliencyMapWindow extends JPanel {
     }
 
     private int[] getTargetClassIDs() {
-        return classSelectors.stream().mapToInt(ClassSelector::getTargetClass).toArray();
+        return classSelectors.stream().mapToInt(NewClassSelector::getTargetClass).toArray();
     }
 
     private void generateSaliencyMap() {
@@ -301,7 +205,7 @@ public class NewSaliencyMapWindow extends JPanel {
                 setSaliencyImage(get());
 
                 // Pack
-                thisWindow.pack();
+                packWindow();
             }
         };
         worker.execute();
