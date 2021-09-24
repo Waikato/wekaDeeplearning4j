cf9457cc305cd3a0b761e1153818e7b4f498ec37
Rhys Compton <rhys.compton@gmail.com>
2021-09-24 16:45:38 -0400

    Version bump v1.7.1 to v1.7.2

--- cuda-scripts/install-cuda-libs.sh
+++ cuda-scripts/install-cuda-libs.sh
@@ -9,7 +9,7 @@ case "${unameOut}" in
     *)          platform="UNSUPPORTED:${unameOut}"
 esac
 
-version='1.7.1'
+version='1.7.2'
 zip_name="wekaDeeplearning4j-cuda-$cuda_version-$version-$platform-x86_64.zip"
 selected_download=$1
 
